c36a886b4fec36a7461ba8d96174986bf4dc41be
Modernizr <paul.irish+mod@gmail.com>
2009-11-08 21:34:40 -0500

    update to latest CSS gradients syntax. (via Ms2ger)

--- modernizr.js
+++ modernizr.js
@@ -302,11 +302,12 @@ window.Modernizr = (function(window,doc){
 		 * http://webkit.org/blog/175/introducing-css-gradients/
 		 * https://developer.mozilla.org/en/CSS/-moz-linear-gradient
 		 * https://developer.mozilla.org/en/CSS/-moz-radial-gradient
+	  	 * http://dev.w3.org/csswg/css3-images/#gradients-
 		 */
 		
 		var str1 = background + '-image:',
 			str2 = 'gradient(linear,left top,right bottom,from(#9f9),to(white));',
-			str3 = 'linear-gradient(left top,right bottom,from(#9f9),to(white));';
+			str3 = 'linear-gradient(left top,#9f9, white);';
 		
 		set_css(
 				str1 + str2
