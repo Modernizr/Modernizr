b06c0305c87acc9ae42effabcfcf109dbd2c79f6
PhistucK <sorrybi@hotmail.com>
2009-12-08 01:03:46 +0800

    made tests for appCache support more thorough

--- modernizr.js
+++ modernizr.js
@@ -556,7 +556,8 @@ window.Modernizr = (function(window,doc){
     };
 
     tests[applicationCache] =  function() {
-        return !!window.applicationCache;
+        var cache = window.applicationCache;
+        return !!(cache && (typeof cache.status != 'undefined') && (typeof cache.update == 'function') && (typeof cache.swapCache == 'function'));
     };
  
 
