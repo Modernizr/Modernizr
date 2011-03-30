55d0c29fe3d0536714054338a95baa6b0500c473
PhistucK <sorrybi@hotmail.com>
2011-03-30 12:37:53 -0700

    Sleeker indexOf evaluation. Minor.

--- modernizr.js
+++ modernizr.js
@@ -229,7 +229,7 @@ window.Modernizr = (function(window,document,undefined){
      * contains returns a boolean for if substr is found within str.
      */
     function contains( str, substr ) {
-        return (''+str).indexOf( substr ) !== -1;
+        return ~(''+str).indexOf( substr );
     }
 
     /**
