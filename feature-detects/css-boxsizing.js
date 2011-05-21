3b9e1afa44ba83df34914f15786b6c784583d66e
PhistucK <sorrybi@hotmail.com>
2011-05-21 15:15:10 -0700

    box-sizing test. fixes #248

---
+++ feature-detects/css-boxsizing.js
@@ -0,0 +1,9 @@
+
+// developer.mozilla.org/en/CSS/box-sizing
+// github.com/Modernizr/Modernizr/issues/248
+
+Modernizr.addTest("boxsizing",function(){
+    return Modernizr.testAllProps("box-sizing");
+});
+
+
