62b4e62e14c01ab5737756e8a73f6a80b8daccf2
Paul Irish <paul.irish@gmail.com>
2011-06-02 13:38:45 -0700

    hosted page update to match whats in master. test suite. so sweet.

--- test/js/test-setup.js
+++ test/js/test-setup.js
@@ -1,5 +1,4 @@
 
-
 // test helper object
 window.TEST = {
   // note some unique members of the Modernizr object
@@ -29,54 +28,4 @@ window.TEST = {
     return str.replace(/^\s*/, "").replace(/\s*$/, "");
   }
 };
-
-
-// Object.keys()
-if (!Object.keys) Object.keys = function(o){
-  if (o !== Object(o)) throw new TypeError('Object.keys called on non-object');
-  var ret=[], p;
-  for (p in o) if(Object.prototype.hasOwnProperty.call(o,p)) ret.push(p);
-  return ret;
-};
-
-
-// Array.prototype.indexOf
-if (!Array.prototype.indexOf){
-  Array.prototype.indexOf = function(searchElement /*, fromIndex */)
-  {
-    "use strict";
-
-    if (this === void 0 || this === null)
-      throw new TypeError();
-
-    var t = Object(this);
-    var len = t.length >>> 0;
-    if (len === 0)
-      return -1;
-
-    var n = 0;
-    if (arguments.length > 0)
-    {
-      n = Number(arguments[1]);
-      if (n !== n) // shortcut for verifying if it's NaN
-        n = 0;
-      else if (n !== 0 && n !== (1 / 0) && n !== -(1 / 0))
-        n = (n > 0 || -1) * Math.floor(Math.abs(n));
-    }
-
-    if (n >= len)
-      return -1;
-
-    var k = n >= 0
-          ? n
-          : Math.max(len - Math.abs(n), 0);
-
-    for (; k < len; k++)
-    {
-      if (k in t && t[k] === searchElement)
-        return k;
-    }
-    return -1;
-  };
-}
 
