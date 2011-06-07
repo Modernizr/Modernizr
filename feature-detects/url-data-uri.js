79592bebc001223678837adf51a48c5c91072c9a
PhistucK <sorrybi@hotmail.com>
2011-06-07 16:58:36 -0700

    data uri test by phil wareham and weston ruter. fixes #14. fixes #294

---
+++ feature-detects/url-data-uri.js
@@ -0,0 +1,21 @@
+// data uri test.
+// https://github.com/Modernizr/Modernizr/issues/14
+
+
+// This test is ascynchronous. Watch out.
+
+(function(){
+
+  var datauri = new Image();
+
+
+  datauri.onerror = function() {
+      Modernizr.addTest('datauri', function () { return false; });
+  };  
+  datauri.onload = function() {
+      Modernizr.addTest('datauri', function () { return (datauri.width == 1 && datauri.height == 1); });
+  };
+
+  datauri.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+
+})();
