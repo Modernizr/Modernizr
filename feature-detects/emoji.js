c7193ff5d33d81af93f87d6add900d6831028c0d
PhistucK <sorrybi@hotmail.com>
2011-10-17 12:55:22 +0300

    Use `Modernizr.canvastext` instead of reinventing the wheel in the emoji feature detect.

--- feature-detects/emoji.js
+++ feature-detects/emoji.js
@@ -1,9 +1,11 @@
+// Requires a Modernizr build with `canvastest` included
+// http://www.modernizr.com/download/#-canvas-canvastext
 Modernizr.addTest('emoji', function() {
-  var node = document.createElement('canvas');
-  if (!(node.getContext && node.getContext('2d'))) return false;
-  var ctx = node.getContext('2d');
-  ctx.textBaseline = "top";
-  ctx.font = "32px Arial";
-  ctx.fillText("😃", 0, 0);
+  if (!Modernizr.canvastext) return false;
+  var node = document.createElement('canvas'),
+      ctx = node.getContext('2d');
+  ctx.textBaseline = 'top';
+  ctx.font = '32px Arial';
+  ctx.fillText('\ud83d\ude03', 0, 0); // "smiling face with open mouth" emoji
   return ctx.getImageData(16, 16, 1, 1).data[0] != 0;
 });
