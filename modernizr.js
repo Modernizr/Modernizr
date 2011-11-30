4b5ee4453714ca3d3a2ef69ca547f9ee76758298
PhistucK <sorrybi@hotmail.com>
2011-11-30 14:43:10 -0500

    Do NOT cast `test` to a Boolean. Truthy/falsy values are okay and casting will break some of the built in tests that use a Boolean *object* with added properties (the audio/video tests do this).
    Fun fact: `!!new Boolean(false) === true`

--- modernizr.js
+++ modernizr.js
@@ -902,7 +902,7 @@ window.Modernizr = (function( window, document, undefined ) {
          test = typeof test == 'function' ? test() : test;
 
          docElement.className += ' ' + (test ? '' : 'no-') + feature;
-         Modernizr[feature] = !!test;
+         Modernizr[feature] = test;
 
        }
 
