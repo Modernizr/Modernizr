9c732ec8abf83c7c92a02221c8aed17de11d814c
PhistucK <sorrybi@hotmail.com>
2011-12-03 11:41:19 -0500

    adding back updated getusermedia.js test

---
+++ feature-detects/getusermedia.js
@@ -0,0 +1,4 @@
+// getUserMedia
+// http://www.whatwg.org/specs/web-apps/current-work/multipage/video-conferencing-and-peer-to-peer-communication.html
+// By Addy Osmani
+Modernizr.addTest('getusermedia', !!('getUserMedia' in navigator));
