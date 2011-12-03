1cba3f56be0f4db85bb9ed9bde3b984bce8ee20a
PhistucK <sorrybi@hotmail.com>
2011-12-03 11:37:56 -0500

    changing test names to lowercase, minor update to structure of apng test, case change for getusermedia

--- feature-detects/classlist.js
+++ feature-detects/classlist.js
@@ -1,4 +1,4 @@
 // classList
 // https://developer.mozilla.org/en/DOM/element.classList
 // By Addy Osmani
-Modernizr.addTest('classList', !!('classList' in document.createElement('a')));
+Modernizr.addTest('classlist', !!('classList' in document.createElement('a')));
--- feature-detects/dataview-api.js
+++ feature-detects/dataview-api.js
@@ -1,4 +1,4 @@
 // DataView 
 // https://developer.mozilla.org/en/JavaScript_typed_arrays/DataView
 // By Addy Osmani
-Modernizr.addTest('dataView', (typeof DataView !== 'undefined' && 'getFloat64' in DataView.prototype));
+Modernizr.addTest('dataview', (typeof DataView !== 'undefined' && 'getFloat64' in DataView.prototype));
