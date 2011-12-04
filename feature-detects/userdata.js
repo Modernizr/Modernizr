fdb020dc07b187096eb97175cb06ef1c557e4a79
Paul Irish <paul.irish@gmail.com>
2011-12-04 09:51:47 -0800

    userdata test. more metadata.

--- feature-detects/userdata.js
+++ feature-detects/userdata.js
@@ -1,4 +1,6 @@
-//test if IE userdata supported
+// test if IE userdata supported
+// msdn.microsoft.com/en-us/library/ms531424(v=vs.85).aspx
+// test by @stereobooster
 
 Modernizr.addTest('userdata', function(){
   return !!document.createElement('div').addBehavior;
