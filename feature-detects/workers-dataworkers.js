7ff703338d8664578a4fd6711cab2188fce7a76d
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2011-12-04 19:56:24 +0200

    Fixed some memory leaking

--- feature-detects/workers-dataworkers.js
+++ feature-detects/workers-dataworkers.js
@@ -11,12 +11,15 @@
         worker  = new Worker('data:text/javascript;base64,dGhpcy5vbm1lc3NhZ2U9ZnVuY3Rpb24oZSl7cG9zdE1lc3NhZ2UoZS5kYXRhKX0=');
 
     worker.onmessage = function(e) {
+      worker.terminate();
       Modernizr.addTest('dataworkers', data === e.data);
+      worker = null;
     };
 
     // Just in case...
     worker.onerror = function() {
       Modernizr.addTest('dataworkers', false);
+      worker = null;
     };
 
     setTimeout(function() {
