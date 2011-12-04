1f97f31cdbf834c797a286e427b3f46da5958e5f
PhistucK <sorrybi@hotmail.com>
2011-12-04 09:45:22 -0800

    simplification of some of addy's new tests in #419

--- feature-detects/gamepad.js
+++ feature-detects/gamepad.js
@@ -3,4 +3,4 @@
 // Note: this feature detection test has been confirmed with the developers
 // of the GamePad API implementation in FF
 // By Addy Osmani
-Modernizr.addTest('gamepad', !!('gamepads' in navigator));
+Modernizr.addTest('gamepad', 'gamepads' in navigator);
