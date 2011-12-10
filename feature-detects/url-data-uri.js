0099fa5992f098a88052dd21227090930d73fe39
PhistucK <sorrybi@hotmail.com>
2011-12-10 15:49:36 -0800

    datauri test. added note about IE7/SSL. fixes #362

--- feature-detects/url-data-uri.js
+++ feature-detects/url-data-uri.js
@@ -1,8 +1,13 @@
 // data uri test.
 // https://github.com/Modernizr/Modernizr/issues/14
 
-
 // This test is asynchronous. Watch out.
+
+
+// in IE7 in HTTPS this can cause a Mixed Content security popup. 
+//  github.com/Modernizr/Modernizr/issues/362
+// To avoid that you can create a new iframe and inject this.. perhaps..
+
 
 (function(){
 
