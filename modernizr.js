17bac93f384e500ca9bd2ea7816373ef52d5b149
PhistucK <sorrybi@hotmail.com>
2011-12-10 14:30:20 -0800

    IE9 fixed the bug where audio codec was neccesary on the video canPlayType call. thx msft! fixes #346

--- modernizr.js
+++ modernizr.js
@@ -627,10 +627,7 @@ window.Modernizr = (function( window, document, undefined ) {
                 bool      = new Boolean(bool);
                 bool.ogg  = elem.canPlayType('video/ogg; codecs="theora"');
 
-                // Workaround required for IE9, which doesn't report video support without audio codec specified.
-                //   bug 599718 @ msft connect
-                var h264 = 'video/mp4; codecs="avc1.42E01E';
-                bool.h264 = elem.canPlayType(h264 + '"') || elem.canPlayType(h264 + ', mp4a.40.2"');
+                bool.h264 = elem.canPlayType('video/mp4; codecs="avc1.42E01E"');
 
                 bool.webm = elem.canPlayType('video/webm; codecs="vp8, vorbis"');
             }
