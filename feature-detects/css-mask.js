f209569b42f6d5a050d04463a84d94212db1a537
PhistucK <sorrybi@hotmail.com>
2011-12-11 19:26:14 -0800

    add css mask detect. fixes #208

---
+++ feature-detects/css-mask.js
@@ -0,0 +1,10 @@
+
+
+// Non-standard feature available in webkit only, so far.
+
+// http://www.webkit.org/blog/181/css-masks/
+// http://developer.apple.com/library/safari/#documentation/InternetWeb/Conceptual/SafariVisualEffectsProgGuide/Masks/Masks.html
+
+// Can combine with clippaths for awesomeness: http://generic.cx/for/webkit/test.html
+
+Modernizr.addTest('cssmask', Modernizr.testAllProps('mask'));
