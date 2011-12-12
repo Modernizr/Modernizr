d5e42f4feec78e14c6dc669b4b0655d517edc751
PhistucK <sorrybi@hotmail.com>
2011-12-11 20:51:01 -0800

    totally redid cookies test. thx @tauren. fixes 191

--- feature-detects/cookies.js
+++ feature-detects/cookies.js
@@ -1,33 +1,15 @@
 
-// cookies?
-// both delicious and useful.
+// by tauren
+// https://github.com/Modernizr/Modernizr/issues/191
 
-(function(){
+Modernizr.addTest('cookies', function () {
+  // Quick test if browser has cookieEnabled host property
+  if (navigator.cookieEnabled) return true;
+  // Create cookie
+  document.cookie = "cookietest=1";
+  var ret = document.cookie.indexOf("cookietest=") != -1;
+  // Delete cookie
+  document.cookie = "cookietest=1; expires=Thu, 01-Jan-1970 00:00:01 GMT";
+  return ret;
+});
 
-// FIXME: we could do this with less code.
-function createCookie(name,value) {
-	document.cookie = name+"="+value+"; path=/";
-}
-
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
-
-function eraseCookie(name) {
-	createCookie(name,"",-1);
-}
-
-
-var random = '' + Math.round(Math.random() * 1e8);
-createCookie('Modernizr', random);
-Modernizr.cookies = readCookie('Modernizr') == random;
-eraseCookie('Modernizr');
-
-})();
