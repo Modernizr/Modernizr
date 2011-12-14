9b91d4361dc07fb1ae16fe5f508f9fe84fd3ee9c
PhistucK <sorrybi@hotmail.com>
2011-12-14 10:38:22 -0800

    note added about androids weak keyframe animation impl. cc @daneden.

--- modernizr.js
+++ modernizr.js
@@ -523,6 +523,9 @@ window.Modernizr = (function( window, document, undefined ) {
     };
 
 
+    // Note, Android < 4 will pass this test, but can only animate 
+    //   a single property at a time
+    //   daneden.me/2011/12/putting-up-with-androids-bullshit/
     tests['cssanimations'] = function() {
         return testPropsAll('animationName');
     };
