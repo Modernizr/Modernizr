8da449d4f9a50f68ecd7e52754385d3ed79e66c2
PhistucK <sorrybi@hotmail.com>
2011-12-16 18:13:18 +0100

    Added the script defer and script async attribute tests

---
+++ feature-detects/script-async.js
@@ -0,0 +1,3 @@
+// async script
+// By Theodoor van Donge
+Modernizr.addTest('script-async', 'async' in document.createElement('script'));
---
+++ feature-detects/script-defer.js
@@ -0,0 +1,3 @@
+// defer script
+// By Theodoor van Donge
+Modernizr.addTest('script-defer', 'defer' in document.createElement('script'));
