efedfee62b2f3eb1cf33a4489e59c9145dc11926
Edwin Martin <edwin@bitstorm.org>
2011-12-20 23:35:07 +0100

    The regex checked for 2 or 3 instead of 3 and 4. Only 3 and 4 are 2G and 3G networks.

--- feature-detects/network-connection.js
+++ feature-detects/network-connection.js
@@ -18,5 +18,5 @@ Modernizr.addTest('lowbandwidth', function() {
 
   return connection.type == 3 // connection.CELL_2G 
       || connection.type == 4 // connection.CELL_3G
-      || /^[23]g$/.test(connection.type); // string value in new spec
+      || /^[34]g$/.test(connection.type); // string value in new spec
 });
