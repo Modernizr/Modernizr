68017f9a5ffd58a019de4d253ff8bc15fcaae8e6
PhistucK <sorrybi@hotmail.com>
2012-01-18 08:22:45 +0900

    Update to use  Modernizr.prefixed() and remove performance memory.

--- feature-detects/performance-memory.js
+++
@@ -1,5 +0,0 @@
-// Navigation Timing (Performance)
-// http://www.html5rocks.com/en/tutorials/webperformance/basics/
-// Currently only available in Chrome
-// By Scott Murphy (uxder)
-Modernizr.addTest('performancememory', !!(window.performance.memory));
--- feature-detects/performance.js
+++ feature-detects/performance.js
@@ -2,4 +2,4 @@
 // https://dvcs.w3.org/hg/webperf/raw-file/tip/specs/NavigationTiming/
 // http://www.html5rocks.com/en/tutorials/webperformance/basics/
 // By Scott Murphy (uxder)
-Modernizr.addTest('performance', !!(window.performance));
+Modernizr.addTest('performance', !!Modernizr.prefixed('performance', window));
