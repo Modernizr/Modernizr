4d4b47f6e47dcd4757a7b6a38f9313c35cb31ebb
PhistucK <sorrybi@hotmail.com>
2012-01-17 11:18:42 +0100

    Updated notifications with Modernizr.prefixed(Nofitications, window)

--- feature-detects/notification.js
+++ feature-detects/notification.js
@@ -1,9 +1,9 @@
 // Notifications
 // By Theodoor van Donge
 
-// webkitNotification is only used by Chrome 
+// window.webkitNotifications is only used by Chrome 
 //	http://www.html5rocks.com/en/tutorials/notifications/quick/
-// Notification only exist in the draft specs 
+// window.Notification only exist in the draft specs 
 //	http://dev.w3.org/2006/webapi/WebNotifications/publish/Notifications.html#idl-if-Notification
 
-Modernizr.addTest('notification', !!window.webkitNotifications || !!window.Notification);
+Modernizr.addTest('notification', !!Modernizr.prefixed('Notifications', window) || !!window.Notification);
