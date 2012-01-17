d50da01083373f6a4ad60edbc1c6b2cc9ccf3194
PhistucK <sorrybi@hotmail.com>
2012-01-17 22:28:07 +0900

    Added Navigation API (performance) and performance memory

---
+++ feature-detects/performance-memory.js
@@ -0,0 +1,5 @@
+// Navigation Timing (Performance)
+// http://www.html5rocks.com/en/tutorials/webperformance/basics/
+// Currently only available in Chrome
+// By Scott Murphy (uxder)
+Modernizr.addTest('performancememory', !!(window.performance.memory));
