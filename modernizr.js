bd7045762db69d4ac79776d63d813f48f9615e16
PhistucK <sorrybi@hotmail.com>
2012-01-29 17:39:49 -0800

    html5shiv v3.1. added. fixes #426

--- modernizr.js
+++ modernizr.js
@@ -917,150 +917,108 @@ window.Modernizr = (function( window, document, undefined ) {
     modElem = inputElem = null;
 
     //>>BEGIN IEPP
-    // Enable HTML 5 elements for styling (and printing) in IE.
-    if ( window.attachEvent && (function(){ var elem = document.createElement('div');
-                                            elem.innerHTML = '<elem></elem>';
-                                            return elem.childNodes.length !== 1; })() ) {
-                                              
-        // iepp v2.2 MIT/GPL2 @jon_neal & afarkas  : github.com/aFarkas/iepp/ 
-        (function(win, doc) {
+    // Enable HTML 5 elements for styling in IE.
 
-          win.iepp = win.iepp || {};
-          var iepp = win.iepp,
-            elems = iepp.html5elements || 'abbr|article|aside|audio|canvas|datalist|details|figcaption|figure|footer|header|hgroup|mark|meter|nav|output|progress|section|subline|summary|time|video',
-            elemsArr = elems.split('|'),
-            elemsArrLen = elemsArr.length,
-            elemRegExp = new RegExp('(^|\\s)('+elems+')', 'gi'), 
-            tagRegExp = new RegExp('<(\/*)('+elems+')', 'gi'),
-            filterReg = /^\s*[\{\}]\s*$/,
-            ruleRegExp = new RegExp('(^|[^\\n]*?\\s)('+elems+')([^\\n]*)({[\\n\\w\\W]*?})', 'gi'),
-            nonPrintMedias = /@media +(?![Print|All])[^{]+\{([^{}]+\{[^{}]+\})+[^}]+\}/g,
-            docFrag = doc.createDocumentFragment(),
-            html = doc.documentElement,
-            head = doc.getElementsByTagName('script')[0].parentNode,
-            bodyElem = doc.createElement('body'),
-            styleElem = doc.createElement('style'),
-            printMedias = /print|all/,
-            body;
+    /*! HTML5 Shiv v3.1 | @jon_neal @afarkas @rem | MIT/GPL2 Licensed */
+    (function (win, doc) {
+      // feature detection: whether the browser supports unknown elements
+      var supportsUnknownElements = (function (a) {
+        a.innerHTML = '<x-element></x-element>';
+        return a.childNodes.length === 1;
+      })(doc.createElement('a'));
 
-          function shim(doc) {
-            var a = -1;
-            while (++a < elemsArrLen) {
-              // Use createElement so IE allows HTML5-named elements in a document
-              doc.createElement(elemsArr[a]);
-            }
-          }
-          
-          iepp.getCSS = function(styleSheetList, mediaType) {
-            try {
-              if(styleSheetList+'' === undefined){
-                return '';
-              }
-            } catch(er){
-              return '';
-            }
-            var a = -1,
-              len = styleSheetList.length,
-              styleSheet,
-              cssText,
-              cssTextArr = [];
-            while (++a < len) {
-              styleSheet = styleSheetList[a];
-              //currently no test for disabled/alternate stylesheets
-              if(styleSheet.disabled){
-                continue;
-              }
-              mediaType = styleSheet.media || mediaType;
-              // Get css from all non-screen stylesheets and their imports
-              if (printMedias.test(mediaType)){
-                cssText = styleSheet.cssText;
-                if(mediaType != 'print'){
-                  cssText = cssText.replace(nonPrintMedias, '');
-                }
-                cssTextArr.push(iepp.getCSS(styleSheet.imports, mediaType), cssText);
-              }
-              //reset mediaType to all with every new *not imported* stylesheet
-              mediaType = 'all';
-            }
-            return cssTextArr.join('');
-          };
-          
-          iepp.parseCSS = function(cssText) {
-            var cssTextArr = [],
-              rule;
-            while ((rule = ruleRegExp.exec(cssText)) != null){
-              // Replace all html5 element references with iepp substitute classnames
-              cssTextArr.push(( (filterReg.exec(rule[1]) ? '\n' : rule[1]) + rule[2] + rule[3]).replace(elemRegExp, '$1.iepp-$2') + rule[4]);
-            }
-            return cssTextArr.join('\n');
-          };
-          
-          iepp.writeHTML = function() {
-            var a = -1;
-            body = body || doc.body;
-            while (++a < elemsArrLen) {
-              var nodeList = doc.getElementsByTagName(elemsArr[a]),
-                nodeListLen = nodeList.length,
-                b = -1;
-              while (++b < nodeListLen){
-                if (nodeList[b].className.indexOf('iepp-') < 0){
-                  // Append iepp substitute classnames to all html5 elements
-                  nodeList[b].className += ' iepp-'+elemsArr[a];
-                }
-              }
-                
-            }
-            docFrag.appendChild(body);
-            html.appendChild(bodyElem);
-            // Write iepp substitute print-safe document
-            bodyElem.className = body.className;
-            bodyElem.id = body.id;
-            // Replace HTML5 elements with <font> which is print-safe and shouldn't conflict since it isn't part of html5
-            bodyElem.innerHTML = body.innerHTML.replace(tagRegExp, '<$1font');
-          };
-          
-          iepp._beforePrint = function() {
-            if(iepp.disablePP){return;}
-            // Write iepp custom print CSS
-            styleElem.styleSheet.cssText = iepp.parseCSS(iepp.getCSS(doc.styleSheets, 'all'));
-            iepp.writeHTML();
-          };
-          
-          iepp.restoreHTML = function() {
-            if(iepp.disablePP){return;}
-            // Undo everything done in onbeforeprint
-            bodyElem.swapNode(body);
-          };
-          
-          iepp._afterPrint = function() {
-            // Undo everything done in onbeforeprint
-            iepp.restoreHTML();
-            styleElem.styleSheet.cssText = '';
-          };
-          
-          // Shim the document and iepp fragment
-          shim(doc);
-          shim(docFrag);
-          
-          //
-          if(iepp.disablePP){
+      // feature detection: whether the browser supports default html5 styles
+      var supportsHtml5Styles = (function (nav, docEl, compStyle) {
+        docEl.appendChild(nav);
+        return (compStyle = (compStyle ? compStyle(nav) : nav.currentStyle).display) && docEl.removeChild(nav) && compStyle === 'block';
+      })(doc.createElement('nav'), doc.documentElement, win.getComputedStyle);
+      
+      // html5 global so that more elements can be shived and also so that existing shiving can be detected on iframes
+      // more elements can be added and shived with the following code: html5.elements.push('element-name'); shivDocument(document);
+      var html5 = {
+        // a list of html5 elements
+        elements: 'abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video'.split(' '),
+        fixDomMethods: true,
+        shivDom: function(scopeDocument){
+          if (scopeDocument.documentShived) {
             return;
           }
+          var
+          documentCreateElement = scopeDocument.createElement,
+          documentCreateDocumentFragment = scopeDocument.createDocumentFragment,
+          documentCreateElementReplaceFunction = function (m) {
+            documentCreateElement(m);
+          };
           
-          // Add iepp custom print style element
-          head.insertBefore(styleElem, head.firstChild);
-          styleElem.media = 'print';
-          styleElem.className = 'iepp-printshim';
-          win.attachEvent(
-            'onbeforeprint',
-            iepp._beforePrint
-          );
-          win.attachEvent(
-            'onafterprint',
-            iepp._afterPrint
-          );
-        })(window, document);
-    }
+          // shiv the document
+          for (var i = 0, l = html5.elements.length; i < l; ++i) {
+            documentCreateElement(html5.elements[i]);
+          }
+
+          // shiv document create element function
+          scopeDocument.createElement = function (nodeName) {
+            var element = documentCreateElement(nodeName);
+            if (html5.fixDomMethods && element.canHaveChildren && !(element.xmlns || element.tagUrn)) {
+              html5.shivDom(element.document);
+            } 
+            return element;
+          };
+
+          // shiv document create document fragment function
+          scopeDocument.createDocumentFragment = function () {
+            var frag = documentCreateDocumentFragment();
+            if(html5.fixDomMethods){
+              html5.shivDom(frag);
+            }
+            return frag;
+          };
+        },
+        // the shiv function
+        shivDocument: function (scopeDocument) {
+          scopeDocument = scopeDocument || doc;
+
+          // test if the document has already been shived
+          if (scopeDocument.documentShived) {
+            return;
+          }
+
+          // set local variables
+          var
+          documentHead = scopeDocument.getElementsByTagName('head')[0];
+
+          // shiv for unknown elements
+          
+          if (!supportsUnknownElements && html5.fixDomMethods) {
+            html5.shivDom(scopeDocument);
+          }
+          scopeDocument.documentShived = true;
+
+          // shiv for default html5 styles
+          if (!supportsHtml5Styles && documentHead) {
+            var div = scopeDocument.createElement('div');
+            div.innerHTML = ['x<style>',
+              'article,aside,details,figcaption,figure,footer,header,hgroup,nav,section{display:block}', // Corrects block display not defined in IE6/7/8/9
+              'audio{display:none}', // Corrects audio display not defined in IE6/7/8/9
+              'canvas,video{display:inline-block;*display:inline;*zoom:1}', // Corrects canvas and video display not defined in IE6/7/8/9 (audio[controls] in IE7)
+              '[hidden]{display:none}audio[controls]{display:inline-block;*display:inline;*zoom:1}', // Corrects 'hidden' attribute and audio[controls] display not present in IE7/8/9
+              'mark{background:#FF0;color:#000}', // Addresses styling not present in IE6/7/8/9
+            '</style>'].join('');
+            documentHead.insertBefore(div.lastChild, documentHead.firstChild);
+          }
+
+          // return document (for potential chaining)
+          return scopeDocument;
+        }
+      };
+
+      // shiv the document
+      html5.shivDocument(doc);
+      
+      html5.type = 'default';
+
+      win.html5 = html5;
+    })(this, document);
+
     //>>END IEPP
 
     // Assign private properties to the return object with prefix
