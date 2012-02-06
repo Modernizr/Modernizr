75de1403fbf324a04cc6008e637d6cb524147fe2
Paul Irish <paul.irish@gmail.com>
2012-02-05 21:30:15 -0800

    more robust prefixed() code and unit tests. ref #478

--- modernizr.js
+++ modernizr.js
@@ -294,13 +294,20 @@ window.Modernizr = (function( window, document, undefined ) {
      */
     function testDOMProps( props, obj, elem ) {
         for ( var i in props ) {
-            if ( obj[ props[i] ] !== undefined) {
-                        // bind!
-                return elem           ? obj[props[i]].bind(elem) : 
-                        // return the property name as a string
-                       elem === false ? props[i] :
-                        // return the unbound function or obj or value
-                                        obj[props[i]];
+            var item = obj[props[i]];
+            if ( item !== undefined) {
+
+                // return the property name as a string
+                if (elem === false) return props[i];
+
+                // let's bind a function
+                if (is(item, 'function')){
+                  // default to autobind unless override
+                  return item.bind(elem || obj);
+                }
+                
+                // return the unbound function or obj or value
+                return item;
             }
         }
         return false;
