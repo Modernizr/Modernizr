f54afd963f52d07f00e3b122e465f26122085fa9
Paul Irish <paul.irish@gmail.com>
2012-02-06 17:25:03 -0800

    tagging 2.5.1

--- modernizr.js
+++ modernizr.js
@@ -1,5 +1,5 @@
 /*!
- * Modernizr v2.5
+ * Modernizr v2.5.1
  * www.modernizr.com
  *
  * Copyright (c) Faruk Ates, Paul Irish, Alex Sexton
@@ -24,7 +24,7 @@
 
 window.Modernizr = (function( window, document, undefined ) {
 
-    var version = '2.5',
+    var version = '2.5.1',
 
     Modernizr = {},
     
