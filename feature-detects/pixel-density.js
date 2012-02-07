40cb3a69f6ee6e4caa5cdcd1a66b5098bed16a7a
Matthew Lein <matthewlein@hotmail.com>
2012-02-07 13:06:31 -0500

    Testing media query if devicePixelRatio is not there

--- feature-detects/pixel-density.js
+++ feature-detects/pixel-density.js
@@ -3,5 +3,28 @@
 // http://www.robertprice.co.uk/robblog/archive/2011/5/Detecting_Retina_Displays_From_JavaScript_And_CSS.shtml
 // http://bradbirdsall.com/mobile-web-in-high-resolution
 // by @matthewlein
-Modernizr.addTest('highres', !!(window.devicePixelRatio > 1.5));
 
+Modernizr.addTest('highres', function() {
+    
+    if ( window.devicePixelRatio ) {
+        return !!( window.devicePixelRatio > 1.5 );
+    } else {
+        // vendor names are all over the board
+        var vendors = [   
+            'min--moz-device-pixel-ratio',
+            '-webkit-min-device-pixel-ratio',
+            'min-device-pixel-ratio'
+        ];
+        var isHighRes = false;
+        
+        // loop through vendors, checking non-prefixed first
+        for (var i = vendors.length - 1; i >= 0; i--) {
+            isHighRes = Modernizr.mq( 'only screen and ('+ vendors[i] + ':1.5)' )
+            if ( isHighRes ) {
+                break;
+            }
+        }
+        return isHighRes;
+    }
+    
+})
