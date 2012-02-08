b101b4337fa6da931353466f6c35b71c77367183
Paul Irish <paul.irish@gmail.com>
2012-02-07 18:34:14 -0800

    2.5.2 final. includes html5shiv 3.3

--- modernizr.js
+++ modernizr.js
@@ -1,5 +1,5 @@
 /*!
- * Modernizr v2.5.2pre
+ * Modernizr v2.5.2
  * www.modernizr.com
  *
  * Copyright (c) Faruk Ates, Paul Irish, Alex Sexton
@@ -24,7 +24,7 @@
 
 window.Modernizr = (function( window, document, undefined ) {
 
-    var version = '2.5.2pre',
+    var version = '2.5.2',
 
     Modernizr = {},
     
