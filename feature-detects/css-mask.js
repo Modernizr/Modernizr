a669575396a2bf3ed6c45a98aedeae745be8275c
PhistucK <sorrybi@hotmail.com>
2012-03-02 23:23:12 -0800

    browser support deets on css-mask

--- feature-detects/css-mask.js
+++ feature-detects/css-mask.js
@@ -1,9 +1,12 @@
 
-
-// Non-standard feature available in webkit only, so far.
+// this tests passes for webkit's proprietary `-webkit-mask` feature
+// as well as mozilla's implementation of `mask` for SVG
 
 // http://www.webkit.org/blog/181/css-masks/
 // http://developer.apple.com/library/safari/#documentation/InternetWeb/Conceptual/SafariVisualEffectsProgGuide/Masks/Masks.html
+
+// https://developer.mozilla.org/en/CSS/mask
+// https://developer.mozilla.org/En/Applying_SVG_effects_to_HTML_content
 
 // Can combine with clippaths for awesomeness: http://generic.cx/for/webkit/test.html
 
