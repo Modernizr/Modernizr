300ad3ccc4912311c3be0078ffe0c01d5a88d1b3
Darius Morawiec <darius@Blumentopf.local>
2012-03-28 00:02:41 +0200

    refactoring with use of the smart prefixed() function

--- feature-detects/vibrate.js
+++ feature-detects/vibrate.js
@@ -1,9 +1,4 @@
+// Vibration API
+// http://www.w3.org/TR/vibration/
 // https://developer.mozilla.org/en/DOM/window.navigator.mozVibrate
-
-Modernizr.addTest('vibrate',function(){
-    for (p in Modernizr._domPrefixes){
-        if( window.navigator[Modernizr._domPrefixes[p].toLowerCase()+'Vibrate'])
-            return true;
-    }
-    return !!window.navigator['vibrate'] || false;
-});
+Modernizr.addTest('vibrate', !!Modernizr.prefixed('vibrate', navigator));
