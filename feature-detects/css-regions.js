c2b0c877c4c43a0d221d78b8aa6706b1fe4a8ddc
Mihai Balan <mibalan@adobe.com>
2012-03-31 01:15:48 +0300

    Reduced version, based on CSS parsing only

--- feature-detects/css-regions.js
+++ feature-detects/css-regions.js
@@ -6,33 +6,3 @@
 Modernizr.addTest('regions',
 	Modernizr.testAllProps('flowInto')
 );
-
-// More complete, behavior based version
-Modernizr.addTest('regions', function() {
-	if (Modernizr.testAllProps('flowInto')) {
-		var style = 'position: fixed; width: 20px; height: 20px; visibility: hidden;';
-		var contentStyle = 'top: -50px; left: -50px;';
-		var regionStyle = 'top: -100px; left: 0px;';
-
-		var contentDiv = document.createElement('div');
-		contentDiv.style.cssText = style + contentStyle;
-		contentDiv.style[Modernizr.prefixed('flowInto')] = 'f1';
-		
-		var regionDiv = document.createElement('div');
-		regionDiv.style.cssText = style + regionStyle;
-		regionDiv.style[Modernizr.prefixed('flowFrom')] = 'f1';
-
-		var contentRect = contentDiv.getBoundingClientRect();
-
-		delete contentDiv;
-		delete regionDiv;
-
-		if (contentRect.left < 0) {
-			return false;
-		} else {
-			return true;
-		}
-	} else {
-		return false;
-	}
-});
