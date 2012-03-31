e594f5b29ebfb2262022fa75c562828def01fe8b
PhistucK <sorrybi@hotmail.com>
2012-03-31 23:13:57 +0100

    adding blobbuilder feature detect test

---
+++ feature-detects/blobbuilder.js
@@ -0,0 +1,4 @@
+// BlobBuilder
+// http://dev.w3.org/2009/dap/file-system/file-writer.html#idl-def-BlobBuilder
+// by Addy Osmani
+Modernizr.addTest('blobbuilder', BlobBuilder || WebKitBlobBuilder);
