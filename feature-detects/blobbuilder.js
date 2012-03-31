fa6bba9c0d8164eec8d8aff3c77622182e1d6203
addyosmani <addyosmani@gmail.com>
2012-03-31 23:40:20 +0100

    revision to include support for moz

--- feature-detects/blobbuilder.js
+++ feature-detects/blobbuilder.js
@@ -1,4 +1,4 @@
 // BlobBuilder
 // http://dev.w3.org/2009/dap/file-system/file-writer.html#idl-def-BlobBuilder
 // by Addy Osmani
-Modernizr.addTest('blobbuilder', BlobBuilder || WebKitBlobBuilder);
+Modernizr.addTest('blobbuilder', window.MozBlobBuilder || window.WebKitBlobBuilder || window.BlobBuilder);
