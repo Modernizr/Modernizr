ac8c5c0666eb797724aa5697a58812310e1d14a1
Eric Bidelman <ebidel@gmail.com>
2012-04-15 14:19:34 -0700

    Fixing busted tests

--- feature-detects/getusermedia.js
+++ feature-detects/getusermedia.js
@@ -1,4 +1,5 @@
 // getUserMedia
 // http://www.whatwg.org/specs/web-apps/current-work/multipage/video-conferencing-and-peer-to-peer-communication.html
-// By Addy Osmani
-Modernizr.addTest('getusermedia', !!('getUserMedia' in navigator));
+// By Eric Bidelman
+
+Modernizr.addTest('getusermedia', !!Modernizr.prefixed('getUserMedia', navigator));
