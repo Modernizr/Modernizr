58c260204fe5f1414bafa2441ccbbf7c20f370dd
Paul Irish <paul.irish@gmail.com>
2012-04-16 17:47:39 -0700

    docs on h264 without quicktime. fixes #546

--- modernizr.js
+++ modernizr.js
@@ -735,6 +735,7 @@ window.Modernizr = (function( window, document, undefined ) {
                 bool      = new Boolean(bool);
                 bool.ogg  = elem.canPlayType('video/ogg; codecs="theora"')      .replace(/^no$/,'');
 
+                // Without QuickTime, this value will be `undefined`. github.com/Modernizr/Modernizr/issues/546
                 bool.h264 = elem.canPlayType('video/mp4; codecs="avc1.42E01E"') .replace(/^no$/,'');
 
                 bool.webm = elem.canPlayType('video/webm; codecs="vp8, vorbis"').replace(/^no$/,'');
