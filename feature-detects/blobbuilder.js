7151add6c7f08e54d1f07ddc2759c5477fb472c7
Paul Irish <paul.irish@gmail.com>
2012-04-25 13:48:29 -0700

    blobbuilder test needs some !!

--- feature-detects/blobbuilder.js
+++ feature-detects/blobbuilder.js
@@ -1,4 +1,5 @@
 // BlobBuilder
 // http://dev.w3.org/2009/dap/file-system/file-writer.html#idl-def-BlobBuilder
-// by Addy Osmani
-Modernizr.addTest('blobbuilder', window.MozBlobBuilder || window.WebKitBlobBuilder || window.BlobBuilder);
+
+
+Modernizr.addTest('blobbuilder', !!(window.MozBlobBuilder || window.WebKitBlobBuilder || window.BlobBuilder));
