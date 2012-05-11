314ceda26e03d8be870c9d100178426ff03d33f2
Paul Irish <paul.irish@gmail.com>
2012-05-11 00:32:58 -0700

    fix masks test so that it doesnt count firefox's SVG mask result.

--- feature-detects/css-mask.js
+++ feature-detects/css-mask.js
@@ -1,13 +1,13 @@
 
 // this tests passes for webkit's proprietary `-webkit-mask` feature
-// as well as mozilla's implementation of `mask` for SVG
+//   www.webkit.org/blog/181/css-masks/
+//   developer.apple.com/library/safari/#documentation/InternetWeb/Conceptual/SafariVisualEffectsProgGuide/Masks/Masks.html
 
-// http://www.webkit.org/blog/181/css-masks/
-// http://developer.apple.com/library/safari/#documentation/InternetWeb/Conceptual/SafariVisualEffectsProgGuide/Masks/Masks.html
+// it does not pass mozilla's implementation of `mask` for SVG
 
-// https://developer.mozilla.org/en/CSS/mask
-// https://developer.mozilla.org/En/Applying_SVG_effects_to_HTML_content
+//   developer.mozilla.org/en/CSS/mask
+//   developer.mozilla.org/En/Applying_SVG_effects_to_HTML_content
 
 // Can combine with clippaths for awesomeness: http://generic.cx/for/webkit/test.html
 
-Modernizr.addTest('cssmask', Modernizr.testAllProps('mask'));
+Modernizr.addTest('cssmask', Modernizr.testAllProps('mask-repeat'));
