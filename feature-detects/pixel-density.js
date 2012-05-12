614c91aa2371caefb1db303f9e6dc29dd4851f41
matthewlein <matthewlein@hotmail.com>
2012-05-12 09:22:10 -0400

    its mine again!

--- feature-detects/pixel-density.js
+++ feature-detects/pixel-density.js
@@ -2,7 +2,7 @@
 // High res devices should have devicePixelRatio and a pixel density around 2, otherwise assume low res
 // http://www.robertprice.co.uk/robblog/archive/2011/5/Detecting_Retina_Displays_From_JavaScript_And_CSS.shtml
 // http://bradbirdsall.com/mobile-web-in-high-resolution
-// somewhat inspired by @matthewlein
+// by @matthewlein
 
 Modernizr.addTest('highres', function() {
 
