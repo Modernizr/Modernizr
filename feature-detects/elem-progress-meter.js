b389d0b5476b2452ddcd44e75aaa99823b500dbc
Mike Sherov <mike.sherov@gmail.com>
2012-05-15 18:19:02 +0200

    Close GH-1: Match Modernizr's current style as close as possible!.

--- feature-detects/elem-progress-meter.js
+++ feature-detects/elem-progress-meter.js
@@ -2,10 +2,10 @@
 
 //tests for progressbar-support. All browsers that don't support progressbar returns undefined =)
 Modernizr.addTest("progressbar",function(){
-    return document.createElement('progress').max != undefined;
+    return document.createElement('progress').max !== undefined;
 });
 
 //tests for meter-support. All browsers that don't support meters returns undefined =)
 Modernizr.addTest("meter",function(){
-    return document.createElement('meter').max != undefined;
+    return document.createElement('meter').max !== undefined;
 });
