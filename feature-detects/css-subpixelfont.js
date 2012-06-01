08bd7784352327a1d865540869bc3fa29917e3d0
Rodney Rehm <mail+github@rodneyrehm.de>
2012-06-01 11:50:36 +0200

    optimizing test using Modernizr.testStyles() as suggested by @ryanseddon

--- feature-detects/css-subpixelfont.js
+++ feature-detects/css-subpixelfont.js
@@ -1,31 +1,23 @@
 /*
  * Test for SubPixel Font Rendering
  * (to infer if GDI or DirectWrite is used on Windows)
- * Authors: @derSchepp, @gerritvanaaken, @rodneyrehm, @yatil
+ * Authors: @derSchepp, @gerritvanaaken, @rodneyrehm, @yatil, @ryanseddon
  * Web: https://github.com/gerritvanaaken/subpixeldetect
  */
+Modernizr.addTest('subpixelfont', function() {
+    var bool,
+        styles = "#modernizr{position: absolute; top: -10em; visibility:hidden; font: normal 10px arial;}#subpixel{float: left; font-size: 33.3333%;}";
+    
+    // see https://github.com/Modernizr/Modernizr/blob/master/modernizr.js#L97
+    Modernizr.testStyles(styles, function(elem) {
+        var subpixel = elem.firstChild;
 
-Modernizr.addTest('subpixelfont',  function() {
-	var container = document.createElement('div'),
-		inner = document.createElement('div'),
-		hasSubpixelFontRendering;
+        subpixel.innerHTML = 'This is a text written in Arial';
 
-	// style and content
-	container.style.cssText = 'position: absolute; top: -10em; visibility:hidden; font: normal 10px arial;';
-	inner.style.cssText = 'float: left; font-size: 33.3333%;';
-	inner.textContent = 'This is a text written in Arial';
+        bool = window.getComputedStyle
+            ? window.getComputedStyle(subpixel, null).getPropertyValue("width") !== '44px'
+            : false;   
+    }, 1, ['subpixel']);
 
-	// add test <div>s to the DOM
-	container.appendChild(inner);
-	document.body.appendChild(container);
-
-	// get actual width of text
-	hasSubpixelFontRendering = window.getComputedStyle 
-		? window.getComputedStyle(inner,null).getPropertyValue("width") !== '44px'
-		: false;
-
-	// clean up
-	container.parentNode.removeChild(container);
-
-	return hasSubpixelFontRendering;
+    return bool;
 });
