b54b10fed63d3f6ebba1a4cad923b6282972b6de
John Reading <john@resonancemultimedia.com>
2012-06-25 10:43:15 -0400

    documentmode test for ie10

--- feature-detects/css-filters.js
+++ feature-detects/css-filters.js
@@ -1,8 +1,8 @@
-// https://github.com/Modernizr/Modernizr/issues/614
+// https://github.com/Modernizr/Modernizr/issues/615
 // http://www.inserthtml.com/2012/06/css-filters/
-
+// https://developer.mozilla.org/en/CSS/filter#Gecko_notes
 Modernizr.addTest('cssfilters', function() {
     el = document.createElement('div');
     el.style.cssText = Modernizr._prefixes.join('filter' + ':blur(2px); ');
-    return !!el.style.length;
+    return !!el.style.length && ((document.documentMode === undefined || document.documentMode > 9));
 });
