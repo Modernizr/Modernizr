45bdc31c93781e39bf214cd4dde48abad8cf309f
Paul Irish <paul.irish@gmail.com>
2012-07-19 14:22:50 +0100

    fixup lastchild syntax. fixes #631

--- feature-detects/css-lastchild.js
+++ feature-detects/css-lastchild.js
@@ -2,7 +2,10 @@
 // https://github.com/Modernizr/Modernizr/pull/304
 
 
-Modernizr.testStyles("#modernizr div {width:100px} #modernizr :last-child{width:200px;display:block}", function (elem) {
-    var lastchild = Modernizr.addTest('lastchild', elem.lastChild.offsetWidth > elem.firstChild.offsetWidth);
-}, 2);
+Modernizr.addTest('lastchild', function(){
 
+  return Modernizr.testStyles("#modernizr div {width:100px} #modernizr :last-child{width:200px;display:block}", function (elem) {
+    return elem.lastChild.offsetWidth > elem.firstChild.offsetWidth;
+  }, 2);
+
+});
