5947156ff670b1214e2ade2084731cc4158bace8
PhistucK <sorrybi@hotmail.com>
2012-07-19 12:13:10 +0100

    removing duplicate vibrate and deprecated blobbuilder

--- feature-detects/blobbuilder.js
+++
@@ -1,5 +0,0 @@
-// BlobBuilder
-// http://dev.w3.org/2009/dap/file-system/file-writer.html#idl-def-BlobBuilder
-
-
-Modernizr.addTest('blobbuilder', !!(window.MozBlobBuilder || window.WebKitBlobBuilder || window.BlobBuilder));
