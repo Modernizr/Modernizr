2958cc500bc2db09b80cada7dbd566d7cbba633d
PhistucK <sorrybi@hotmail.com>
2012-07-23 18:08:58 +0200

    Update feature-detects/forms-formattribute.js

--- feature-detects/forms-formattribute.js
+++ feature-detects/forms-formattribute.js
@@ -1,27 +1,30 @@
 
-
 // Detects whether input form="form_id" is available on the platform
 // E.g. IE 10 (and below), don't support this
 
 Modernizr.addTest("formattribute", function(){
-  var form = document.createElement("form"),
-      input = document.createElement("input"),
-      div = document.createElement("div"),
-      id = "formtest",
-      bool = false;
-  
-  form.id = id;
-  input.setAttribute("form",id);
-  
-  div.appendChild(form);
-  div.appendChild(input);
-  
-  document.documentElement.appendChild(div);
-  
-  bool = form.elements.length === 1;
-  
-  div.parentNode.removeChild(div);
-  
-  return bool;
+  try {
+    var form = document.createElement("form"),
+        input = document.createElement("input"),
+        div = document.createElement("div"),
+        id = "formtest",
+        bool = false;
+    
+    form.id = id;
+    input.setAttribute("form",id);
+    
+    div.appendChild(form);
+    div.appendChild(input);
+    
+    document.documentElement.appendChild(div);
+    
+    bool = form.elements.length === 1;
+    
+    div.parentNode.removeChild(div);
+    
+    return bool;
+  } catch (e) {
+    return false;
+  }
   
 });
