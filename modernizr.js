3ee8ac31a268f2073a4feb36a19857c293701038
aFarkas <info@corrupt-system.de>
2012-08-26 14:10:09 +0200

    fix pagereload in Safari if body scrollbars are styled fixes Issue #524

--- modernizr.js
+++ modernizr.js
@@ -96,12 +96,12 @@ window.Modernizr = (function( window, document, undefined ) {
     // Inject element with style element and some CSS rules
     injectElementWithStyles = function( rule, callback, nodes, testnames ) {
 
-      var style, ret, node,
+      var style, ret, node, docOverflow,
           div = document.createElement('div'),
           // After page load injecting a fake body doesn't work so check if body exists
           body = document.body,
           // IE6 and 7 won't return offsetWidth or offsetHeight unless it's in the body element, so we fake it.
-          fakeBody = body ? body : document.createElement('body');
+          fakeBody = body || document.createElement('body');
 
       if ( parseInt(nodes, 10) ) {
           // In order not to give false positives we create a node for each test
@@ -127,12 +127,21 @@ window.Modernizr = (function( window, document, undefined ) {
       if ( !body ) {
           //avoid crashing IE8, if background image is used
           fakeBody.style.background = "";
+          //Safari 5.13/5.1.4 OSX stops loading if ::-webkit-scrollbar is used and scrollbars are visible
+          fakeBody.style.overflow = "hidden";
+          docOverflow = docElement.style.overflow;
+          docElement.style.overflow = 'hidden';
           docElement.appendChild(fakeBody);
       }
 
       ret = callback(div, rule);
       // If this is done after page load we don't want to remove the body so check if body exists
-      !body ? fakeBody.parentNode.removeChild(fakeBody) : div.parentNode.removeChild(div);
+      if ( !body ) {
+          fakeBody.parentNode.removeChild(fakeBody);
+          docElement.style.overflow = docOverflow;
+      } else {
+          div.parentNode.removeChild(div);
+      } 
 
       return !!ret;
 
