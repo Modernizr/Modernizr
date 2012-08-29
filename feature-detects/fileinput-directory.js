94a3810f42b199df70857ad1a3fcb2db4d973376
Alexander Farennikov <afarennikov@centraldesktop.com>
2012-08-29 12:18:27 -0700

    Renamed test file to match naming pattern

---
+++ feature-detects/fileinput-directory.js
@@ -0,0 +1,10 @@
+// Directory
+// By Alexander Farennikov
+
+// Detects `webkitdirectory` or `directory` attributes on <input type="file">
+
+Modernizr.addTest('fileinputdirectory', function(){
+  var el = document.createElement('input');
+  el.type = 'file';
+  return typeof el.webkitdirectory !== "undefined" || typeof el.directory !== "undefined";
+});
