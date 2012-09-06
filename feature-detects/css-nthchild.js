c5f2bf0a20fb40d24c51ea095c509bb6db5bf76e
Emil Rømer Christensen <christensen.emil@gmail.com>
2012-09-06 16:30:13 +0200

    Add nth-child feature-detect

---
+++ feature-detects/css-nthchild.js
@@ -0,0 +1,13 @@
+// nth-child pseudo selector
+Modernizr.addTest('nthchild', function(){
+
+  return Modernizr.testStyles("#modernizr div {width:1px} #modernizr div:nth-child(2n) {width:2px;}", function (elem) {
+    var elems = elem.getElementsByTagName("div"),
+    test = true;
+    for (i=0; i<5; i++) {
+      test = test && elems[i].offsetWidth == i%2+1;
+    }
+    return test;
+  }, 5);
+
+});
