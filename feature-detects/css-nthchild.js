c9645f1d6e5c8b6af4709c46c31a11792e87bfc3
Emil Rømer Christensen <christensen.emil@gmail.com>
2012-09-07 11:35:50 +0300

    Fix indent, single-quotes and leaking i

--- feature-detects/css-nthchild.js
+++ feature-detects/css-nthchild.js
@@ -1,13 +1,15 @@
 // nth-child pseudo selector
 Modernizr.addTest('nthchild', function(){
 
-  return Modernizr.testStyles("#modernizr div {width:1px} #modernizr div:nth-child(2n) {width:2px;}", function (elem) {
-    var elems = elem.getElementsByTagName("div"),
-    test = true;
-    for (i=0; i<5; i++) {
-      test = test && elems[i].offsetWidth == i%2+1;
-    }
-    return test;
-  }, 5);
+    return Modernizr.testStyles('#modernizr div {width:1px} #modernizr div:nth-child(2n) {width:2px;}', function (elem) {
+        var elems = elem.getElementsByTagName('div'),
+            test = true;
+
+        for (var i=0; i<5; i++) {
+            test = test && elems[i].offsetWidth == i%2+1;
+        }
+        
+        return test;
+    }, 5);
 
 });
