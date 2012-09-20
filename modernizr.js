97761626c6ff35410da1717a58442a7f06474778
Chris Ruppel <chris.ruppel@gmail.com>
2012-09-20 00:15:26 -0500

    Moving webgl test to feature-detects/webgl.js

--- modernizr.js
+++ modernizr.js
@@ -400,15 +400,6 @@ window.Modernizr = (function( window, document, undefined ) {
      * -----
      */
 
-    // webk.it/70117 is tracking a legit WebGL feature detect proposal
-
-    // We do a soft detect which may false positive in order to avoid
-    // an expensive context creation: bugzil.la/732441
-
-    tests['webgl'] = function() {
-        return !!window.WebGLRenderingContext;
-    };
-
     /*
      * The Modernizr.touch test only indicates if the browser supports
      *    touch events, which does not necessarily reflect a touchscreen
