8924f6541a699786eb679886f9399d92210dc131
Chris Ruppel <chris.ruppel@gmail.com>
2012-09-20 23:41:48 -0500

    Moving indexedDB test into feature-detects/indexedDB.js

--- modernizr.js
+++ modernizr.js
@@ -400,14 +400,6 @@ window.Modernizr = (function( window, document, undefined ) {
      * -----
      */
 
-    // Vendors had inconsistent prefixing with the experimental Indexed DB:
-    // - Webkit's implementation is accessible through webkitIndexedDB
-    // - Firefox shipped moz_indexedDB before FF4b9, but since then has been mozIndexedDB
-    // For speed, we don't test the legacy (and beta-only) indexedDB
-    tests['indexedDB'] = function() {
-      return !!testPropsAll("indexedDB", window);
-    };
-
     // documentMode logic from YUI to filter out IE8 Compat Mode
     //   which false positives.
     tests['hashchange'] = function() {
