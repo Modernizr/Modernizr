f96871995e689f1cc146bb6ef81efc0286f7ec75
seutje <seutje@gmail.com>
2012-09-21 19:59:00 +0200

    Move audio test to feature-detects/audio.js

--- modernizr.js
+++ modernizr.js
@@ -624,28 +624,6 @@ window.Modernizr = (function( window, document, undefined ) {
         return bool;
     };
 
-    tests['audio'] = function() {
-        var elem = document.createElement('audio'),
-            bool = false;
-
-        try {
-            if ( bool = !!elem.canPlayType ) {
-                bool      = new Boolean(bool);
-                bool.ogg  = elem.canPlayType('audio/ogg; codecs="vorbis"').replace(/^no$/,'');
-                bool.mp3  = elem.canPlayType('audio/mpeg;')               .replace(/^no$/,'');
-
-                // Mimetypes accepted:
-                //   developer.mozilla.org/En/Media_formats_supported_by_the_audio_and_video_elements
-                //   bit.ly/iphoneoscodecs
-                bool.wav  = elem.canPlayType('audio/wav; codecs="1"')     .replace(/^no$/,'');
-                bool.m4a  = ( elem.canPlayType('audio/x-m4a;')            ||
-                              elem.canPlayType('audio/aac;'))             .replace(/^no$/,'');
-            }
-        } catch(e) { }
-
-        return bool;
-    };
-
 
     /*>>webforms*/
     // input features and input types go directly onto the ret object, bypassing the tests loop.
