e574d9c4ce4ae204f90ee2a9d231eeda8d1e9f67
seutje <seutje@gmail.com>
2012-09-21 20:17:36 +0200

    Move cssanimations to feature-detects/css-animations.js

--- modernizr.js
+++ modernizr.js
@@ -482,14 +482,6 @@ window.Modernizr = (function( window, document, undefined ) {
     };
 
 
-    // Note, Android < 4 will pass this test, but can only animate
-    //   a single property at a time
-    //   daneden.me/2011/12/putting-up-with-androids-bullshit/
-    tests['cssanimations'] = function() {
-        return testPropsAll('animationName');
-    };
-
-
     tests['cssgradients'] = function() {
         /**
          * For CSS Gradients syntax, please see:
