7d2a4895340bfe0056c7a9166a12d25c4ff2a6ea
Chris Ruppel <chris.ruppel@gmail.com>
2012-09-20 23:51:10 -0500

    Moving history test into feature-detects/history.js

--- modernizr.js
+++ modernizr.js
@@ -400,15 +400,6 @@ window.Modernizr = (function( window, document, undefined ) {
      * -----
      */
 
-    // Per 1.6:
-    // This used to be Modernizr.historymanagement but the longer
-    // name has been deprecated in favor of a shorter and property-matching one.
-    // The old API is still available in 1.6, but as of 2.0 will throw a warning,
-    // and in the first release thereafter disappear entirely.
-    tests['history'] = function() {
-      return !!(window.history && history.pushState);
-    };
-
     tests['draganddrop'] = function() {
         var div = document.createElement('div');
         return ('draggable' in div) || ('ondragstart' in div && 'ondrop' in div);
