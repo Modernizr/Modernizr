0ee10598316467904238b0820dd77e03dd2a0c2c
seutje <seutje@gmail.com>
2012-09-21 19:49:11 +0200

    Move webworkers test to feature-detects/worker-webworkers.js

--- modernizr.js
+++ modernizr.js
@@ -685,10 +685,6 @@ window.Modernizr = (function( window, document, undefined ) {
     };
 
 
-    tests['webworkers'] = function() {
-        return !!window.Worker;
-    };
-
     /*>>webforms*/
     // input features and input types go directly onto the ret object, bypassing the tests loop.
     // Hold this guy to execute in a moment.
