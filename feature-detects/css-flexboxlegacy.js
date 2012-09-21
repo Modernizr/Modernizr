c88e86b87bc1eed1245f8353c06c7f3638ecf271
Chris Ruppel <chris.ruppel@gmail.com>
2012-09-20 23:14:39 -0500

    Updating flexboxlegacy test to use external method

--- feature-detects/css-flexboxlegacy.js
+++ feature-detects/css-flexboxlegacy.js
@@ -2,4 +2,4 @@
 // The *old* flexbox
 // www.w3.org/TR/2009/WD-css3-flexbox-20090723/
 
-Modernizr.addTest('flexboxlegacy', Modernizr.testPropsAll('boxDirection'));
+Modernizr.addTest('flexboxlegacy', Modernizr.testAllProps('boxDirection'));
