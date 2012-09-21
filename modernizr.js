9c2a235cb36746d3dd156ed7debdc75ddd1ca0b9
Chris Ruppel <chris.ruppel@gmail.com>
2012-09-20 23:08:48 -0500

    Moving geolocation test to feature-detects/geolocation.js

--- modernizr.js
+++ modernizr.js
@@ -400,21 +400,6 @@ window.Modernizr = (function( window, document, undefined ) {
      * -----
      */
 
-    // geolocation is often considered a trivial feature detect...
-    // Turns out, it's quite tricky to get right:
-    //
-    // Using !!navigator.geolocation does two things we don't want. It:
-    //   1. Leaks memory in IE9: github.com/Modernizr/Modernizr/issues/513
-    //   2. Disables page caching in WebKit: webk.it/43956
-    //
-    // Meanwhile, in Firefox < 8, an about:config setting could expose
-    // a false positive that would throw an exception: bugzil.la/688158
-
-    tests['geolocation'] = function() {
-        return 'geolocation' in navigator;
-    };
-
-
     tests['postmessage'] = function() {
       return !!window.postMessage;
     };
