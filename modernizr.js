707d66f89f063b1b8996baca792a051e0f0002b9
Chris Ruppel <chris.ruppel@gmail.com>
2012-09-23 17:17:33 -0500

    Moving multiplebgs test into feature-detects/css-multiplebgs.js

--- modernizr.js
+++ modernizr.js
@@ -400,21 +400,6 @@ window.Modernizr = (function( window, document, undefined ) {
      * -----
      */
 
-    tests['multiplebgs'] = function() {
-        // Setting multiple images AND a color on the background shorthand property
-        //  and then querying the style.background property value for the number of
-        //  occurrences of "url(" is a reliable method for detecting ACTUAL support for this!
-
-        setCss('background:url(https://),url(https://),red url(https://)');
-
-        // If the UA supports multiple backgrounds, there should be three occurrences
-        //   of the string "url(" in the return value for elemStyle.background
-
-        return (/(url\s*\(.*?){3}/).test(mStyle.background);
-    };
-
-
-
     // this will false positive in Opera Mini
     //   github.com/Modernizr/Modernizr/issues/396
 
