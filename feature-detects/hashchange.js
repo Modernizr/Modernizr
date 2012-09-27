55d5b0c0519d2f7370e8b9a30e0e796cc6bf9e52
Paul Irish <paul.irish@gmail.com>
2012-09-26 20:31:27 -0700

    fixup tests

--- feature-detects/hashchange.js
+++ feature-detects/hashchange.js
@@ -1,5 +1,11 @@
 
-// documentMode logic from YUI to filter out IE8 Compat Mode
-//   which false positives.
 
-Modernizr.addTest('hashchange', isEventSupported('hashchange', window) && (document.documentMode === undefined || document.documentMode > 7));
+Modernizr.addTest('hashchange', function(){
+
+  if (Modernizr.hasEvent('hashchange', window) === false) return false;
+
+  // documentMode logic from YUI to filter out IE8 Compat Mode
+  //   which false positives.
+  return (document.documentMode === undefined || document.documentMode > 7);
+
+});
