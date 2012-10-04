3ccffab7b46729e7fb0322caa64ce8df6620b0fd
James Taylor <jt@gosquared.com>
2012-10-04 16:55:08 +0200

    SVG filter detection fix
    Fixes typo in SVG filter detection.

--- feature-detects/svg-filters.js
+++ feature-detects/svg-filters.js
@@ -5,7 +5,7 @@
 Modernizr.addTest('svgfilters', function(){
 	var result = false;
     try {
-      result = typeof SVGFEColorMatrixElement !== undefined &&
+      result = typeof SVGFEColorMatrixElement !== 'undefined' &&
                SVGFEColorMatrixElement.SVG_FECOLORMATRIX_TYPE_SATURATE == 2;
     }
     catch(e) {}
