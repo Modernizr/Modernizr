5d27904647f0f4953c80d736c67bfcbfa8b2bc6f
Jordan Santell <jsantell@gmail.com>
2012-10-07 10:46:50 -0400

    Remove excessive parens

--- feature-detects/audio-audiodata-api.js
+++ feature-detects/audio-audiodata-api.js
@@ -1,4 +1,4 @@
 // Mozilla Audio Data API
 // https://wiki.mozilla.org/Audio_Data_API
 // by Addy Osmani
-Modernizr.addTest('audiodata', !!((new window.Audio()).mozSetup));
+Modernizr.addTest('audiodata', !!(new window.Audio()).mozSetup);
