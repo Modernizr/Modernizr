9eb1b2e02df8dd5df5d4f1fb92e62608393dedc9
Alexander Farennikov <afarennikov@centraldesktop.com>
2012-10-18 17:50:38 -0700

    Updated formatting according to recommendations

--- feature-detects/fileinput-directory.js
+++ feature-detects/fileinput-directory.js
@@ -3,8 +3,8 @@
 
 // Detects `webkitdirectory` or `directory` attributes on <input type="file">
 
-Modernizr.addTest('fileinputdirectory', function(){
+Modernizr.addTest('fileinputdirectory', function() {
   var el = document.createElement('input');
   el.type = 'file';
-  return typeof el.webkitdirectory !== "undefined" || typeof el.directory !== "undefined";
+  return typeof el.webkitdirectory !== 'undefined' || typeof el.directory !== 'undefined';
 });
