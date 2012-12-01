e337a1945cad7fe3cb130a72485f0ce66d4d7890
Alex Sexton <AlexSexton@gmail.com>
2012-12-01 16:31:17 -0600

    Not using the cleanup file anymore. Each test is responsible for adding cleanup to the queue itself.

--- src/cleanup.js
+++
@@ -1,7 +0,0 @@
-define(['setCss'], function( setCss ) {
-  setCss('');
-  // If we can figure out a way to clean up
-  // modElem and inputElem, we should
-  // We _could_ put them on the Modernizr object
-  // for reuse or deletion.
-});
