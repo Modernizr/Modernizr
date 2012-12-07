395369250338fee49b04bbb9077ebf3f0233878f
Alex Sexton <AlexSexton@gmail.com>
2012-12-07 01:16:58 -0600

    SUPERSECRET MICROSOFT REQUESTED SENSORSHIP

--- feature-detects/css/animations.js
+++ feature-detects/css/animations.js
@@ -1,7 +1,7 @@
 define(['Modernizr', 'testAllProps'], function( Modernizr, testAllProps ) {
   // Note, Android < 4 will pass this test, but can only animate
   //   a single property at a time
-  //   daneden.me/2011/12/putting-up-with-androids-bullshit/
+  //   http://goo.gl/CHVJm
   Modernizr.addTest('cssanimations', function() {
     return testAllProps('animationName');
   });
