cbe5b1912849626d1cf80122f2901703d6b99880
Zoltan Horvath <zoltan@webkit.org>
2013-01-10 14:03:42 -0800

    Remove unnecessary feature detection.

--- feature-detects/css-shapeinside.js
+++
@@ -1,4 +0,0 @@
-// http://www.w3.org/TR/css3-exclusions/#shape-inside-property
-// Example: http://www.adobe.com/devnet/html5/articles/css3-regions.html#articlecontentAdobe_numberedheader_0
-
-Modernizr.addTest('shape-inside', Modernizr.testAllProps('shapeInside'));
