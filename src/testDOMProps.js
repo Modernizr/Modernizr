2f3f96b11b795146f06748664087052294e3520d
Steven Allen <steven@stebalien.com>
2013-01-27 18:47:31 -0500

    Catch exceptions when looking up properties in the DOM
    Firefox will sometimes throw an exception when looking up known but
    disabled features in the dom. This commit catches exceptions when
    looking up properties in testDOMProps.

--- src/testDOMProps.js
+++ src/testDOMProps.js
@@ -5,7 +5,12 @@ define(['is', 'fnBind'], function( is ) {
    */
   function testDOMProps( props, obj, elem ) {
     for ( var i in props ) {
-      var item = obj[props[i]];
+      var item;
+      try {
+        item = obj[props[i]];
+      } catch (e) {
+        continue;
+      }
       if ( item !== undefined) {
 
         // return the property name as a string
