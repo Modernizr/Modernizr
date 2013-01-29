e1367ce0479ea79292bcf75d91e306a2d589f7af
Ryan Seddon <seddon.ryan@gmail.com>
2013-01-30 10:16:49 +1100

    Update detect for fullscreen api. Fixes #739

--- feature-detects/fullscreen-api.js
+++ feature-detects/fullscreen-api.js
@@ -1,12 +1,6 @@
 define(['Modernizr', 'domPrefixes'], function( Modernizr, domPrefixes ) {
-  Modernizr.addTest('fullscreen',function() {
-    for (var i = 0; i < domPrefixes.length; i++) {
-      if( document[domPrefixes[i].toLowerCase() + 'CancelFullScreen'])
-        return true;
-    }
-    return !!document['cancelFullScreen'] || false;
-  });
-
   // http://developer.apple.com/library/safari/documentation/AudioVideo/Conceptual/Using_HTML5_Audio_Video/ControllingMediaWithJavaScript/ControllingMediaWithJavaScript.html#//apple_ref/doc/uid/TP40009523-CH3-SW20
   // https://developer.mozilla.org/en/API/Fullscreen
+  // github.com/Modernizr/Modernizr/issues/739
+  Modernizr.addTest('fullscreen', !!(Modernizr.prefixed("exitFullscreen", document, false) || Modernizr.prefixed("cancelFullScreen", document, false)));
 });
