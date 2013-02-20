a60c7278ebb9193883f6a66275e532d2978bd095
Stu Cox <stuart.cox@gmail.com>
2013-02-20 10:01:21 +0000

    Rolled `flexbox` and `flexboxlegacy` detects back to original format (just using `testAllProps()`)

--- feature-detects/css/flexbox.js
+++ feature-detects/css/flexbox.js
@@ -1,12 +1,5 @@
-define(['Modernizr', 'testAllProps', 'nativeCSSDetect'], function( Modernizr, testAllProps, nativeCSSDetect ) {
+define(['Modernizr', 'testAllProps'], function( Modernizr, testAllProps ) {
   // The *new* flexbox
   // dev.w3.org/csswg/css3-flexbox
-  Modernizr.addTest('flexbox', function () {
-    var result = nativeCSSDetect('flex-wrap', 'wrap', true);
-    if (typeof result !== 'undefined') {
-        return result;
-    }
-    // Failing that, do it the old way
-    return testAllProps('flexWrap');
-  });
+  Modernizr.addTest('flexbox', testAllProps('flexWrap'));
 });
--- feature-detects/css/flexboxlegacy.js
+++ feature-detects/css/flexboxlegacy.js
@@ -1,12 +1,5 @@
-define(['Modernizr', 'testAllProps', 'nativeCSSDetect'], function( Modernizr, testAllProps, nativeCSSDetect ) {
+define(['Modernizr', 'testAllProps'], function( Modernizr, testAllProps ) {
   // The *old* flexbox
   // www.w3.org/TR/2009/WD-css3-flexbox-20090723/
-  Modernizr.addTest('flexboxlegacy', function () {
-    var result = nativeCSSDetect('box-direction', 'reverse', true);
-    if (typeof result !== 'undefined') {
-        return result;
-    }
-    // Failing that, do it the old way
-    return testAllProps('boxDirection');
-  });
+  Modernizr.addTest('flexboxlegacy', testAllProps('boxDirection'));
 });
