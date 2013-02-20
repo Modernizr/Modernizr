2372af529deb3cc8795a1c9122ffb0e1f5235c2b
Stu Cox <stuart.cox@gmail.com>
2013-02-20 10:04:23 +0000

    Updated `testProp()` to accept a 2nd `value` arg for native feature detection, and rejigged (and renamed) the native wrapper to suit

--- src/nativeTestProps.js
+++ src/nativeTestProps.js
@@ -1,31 +1,30 @@
 define(['injectElementWithStyles', 'prefixes'], function ( injectElementWithStyles, prefixes ) {
     // Function to allow us to use native feature detection functionality if available.
-    // Follows `(DOMString property, DOMString value)` interface
-    function nativeCSSDetect ( property, value, isPropPrefixed, isValuePrefixed ) {
-        var propPrefixes = isPropPrefixed ? prefixes : [''],
-            valuePrefixes = isValuePrefixed ? prefixes : [''],
-            i = propPrefixes.length;
+    // Accepts a list of property names and a list of values
+    function nativeTestProps ( props, values ) {
+        var i = props.length,
+            j;
         // Start with the JS API: http://www.w3.org/TR/css3-conditional/#the-css-interface
         if ('CSS' in window && 'supports' in window.CSS) {
             // Try every prefixed variant, of both property and value
             while (i--) {
-                j = valuePrefixes.length;
+                j = values.length;
                 while (j--) {
-                    if (window.CSS.supports(prefixes[i] + property, prefixes[j] + value)) {
+                    if (window.CSS.supports(props[i], values[j])) {
                         return true;
                     }
                 }
             }
             return false;
         }
-        // Otherwise fall back to at-rule
+        // Otherwise fall back to at-rule (for FF 17 and Opera 12.x)
         else if ('CSSSupportsRule' in window) {
             // Build a condition string for every prefixed variant
             var conditionText = [];
             while (i--) {
-                j = valuePrefixes.length;
+                j = values.length;
                 while (j--) {
-                    conditionText.push('(' + prefixes[i] + property + ':' + prefixes[j] + value + ')');
+                    conditionText.push('(' + props[i] + ':' + values[j] + ')');
                 }
             }
             conditionText = conditionText.join(' or ');
@@ -39,5 +38,5 @@ define(['injectElementWithStyles', 'prefixes'], function ( injectElementWithStyl
             return undefined;
         }
     }
-    return nativeCSSDetect;
+    return nativeTestProps;
 });
