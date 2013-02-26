ea9d5f3df21fbb5ac951703188913a735e813d76
Stu Cox <stuart.cox@gmail.com>
2013-02-26 19:45:04 +0000

    Revert "Added detect for <template> HTML tag"
    This reverts commit 0252b5203a35289e04445ef5153494b8212649a1.

--- feature-detects/elem/template.js
+++
@@ -1,6 +0,0 @@
-define(['Modernizr', 'createElement'], function( Modernizr, createElement ) {
-  // <template> HTML tag
-  // http://www.html5rocks.com/en/tutorials/webcomponents/template/
-  // https://dvcs.w3.org/hg/webcomponents/raw-file/tip/spec/templates/index.html
-  Modernizr.addTest('template', 'content' in createElement('template'));
-});
