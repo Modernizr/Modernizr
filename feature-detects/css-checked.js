5e7a15fbfd70c766d799ae845dcf75e06edaa30b
Rich Archer <rich.archer@unboxedconsulting.com>
2013-03-02 14:00:13 +0200

    Updating detection to new module style

--- feature-detects/css-checked.js
+++ feature-detects/css-checked.js
@@ -1,15 +1,10 @@
-Modernizr.addTest('csschecked', function () {
-
-  return Modernizr.testStyles("#modernizr input {margin-left:0px;} #modernizr input:checked {margin-left: 20px;}", function (elem) {
-
-    var chx = document.createElement('input');
+define(['Modernizr', 'createElement', 'testStyles'], function (Modernizr, createElement, testStyles) {
+  testStyles("#modernizr input {margin-left:0px;} #modernizr input:checked {margin-left: 20px;}", function (elem) {
+    var chx = createElement('input');
     chx.type = "checkbox";
     chx.checked = "checked";
 
     elem.appendChild(chx);
-
-    return elem.lastChild.offsetLeft >= 20;
-
-  });
-
+    Modernizr.addTest('csschecked', elem.lastChild.offsetLeft >= 20);
+  }, 2);
 });
