e81ed15ab3ddc9818f830f8d2364166cfd9b03a9
Stu Cox <stuart.cox@gmail.com>
2013-03-09 15:08:14 -0600

    Updated other feature detects to use native detection

--- feature-detects/css/borderimage.js
+++ feature-detects/css/borderimage.js
@@ -8,5 +8,5 @@
 }
 !*/
 define(['Modernizr', 'testAllProps'], function( Modernizr, testAllProps ) {
-    Modernizr.addTest('borderimage', testAllProps('borderImageWidth', '2px'));
+    Modernizr.addTest('borderimage', testAllProps('borderImage', 'url()'));
 });
--- feature-detects/css/wrapflow.js
+++ feature-detects/css/wrapflow.js
@@ -1,20 +1,6 @@
-define(['Modernizr', 'prefixed', 'docElement'], function( Modernizr, prefixed, docElement ) {
+define(['Modernizr', 'testAllProps'], function( Modernizr, testAllProps ) {
     // http://www.w3.org/TR/css3-exclusions
     // Examples: http://html.adobe.com/webstandards/cssexclusions
     // Separate test for `wrap-flow` property as IE10 has just implemented this alone
-    Modernizr.addTest('wrapflow', function () {
-        var prefixedProperty = prefixed('wrapFlow'),
-            wrapFlowProperty;
-
-        if (!prefixedProperty)
-            return false;
-
-        wrapFlowProperty = prefixedProperty.replace(/([A-Z])/g, function (str, m1) { return '-' + m1.toLowerCase(); }).replace(/^ms-/, '-ms-');
-
-        return Modernizr.testStyles('#modernizr { ' + wrapFlowProperty + ':start }', function (elem) {
-            // Check against computed value
-            var styleObj = window.getComputedStyle ? getComputedStyle(elem, null) : elem.currentStyle;
-            return styleObj[prefixed('wrapFlow', docElement.style, false)] == 'start';
-        });
-    });
+    Modernizr.addTest('wrapflow', testAllProps('wrapFlow', 'start'));
 });
