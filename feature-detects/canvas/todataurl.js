cd3b2c6a0f15e69aa924f1d95d23feac514519c6
Stu Cox <stu@stucox.co.uk>
2013-03-11 16:13:32 -0500

    Fixed test fails

--- feature-detects/canvas/todataurl.js
+++ feature-detects/canvas/todataurl.js
@@ -12,22 +12,16 @@
 !*/
 define(['Modernizr', 'addTest', 'createElement', 'test/canvas'], function( Modernizr, addTest, createElement ) {
 
-  if (!Modernizr.canvas) {
-    return false;
-  }
-
   var canvas = createElement('canvas');
 
   addTest('todataurljpeg', function() {
-    return canvas.toDataURL('image/jpeg').indexOf('data:image/jpeg') === 0;
+    return !!Modernizr.canvas && canvas.toDataURL('image/jpeg').indexOf('data:image/jpeg') === 0;
   });
   addTest('todataurlpng', function() {
-    return canvas.toDataURL('image/png').indexOf('data:image/png') === 0;
+    return !!Modernizr.canvas && canvas.toDataURL('image/png').indexOf('data:image/png') === 0;
   });
   addTest('todataurlwebp', function() {
-    return canvas.toDataURL('image/webp').indexOf('data:image/webp') === 0;
+    return !!Modernizr.canvas && canvas.toDataURL('image/webp').indexOf('data:image/webp') === 0;
   });
-
-  canvas = undefined;
 
 });
