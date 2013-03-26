a8dc63312c26507a5aa21d7f41b96dac6bb91e0d
Victor Pegado <victor.pegado@kivra.com>
2013-03-26 16:49:29 +0100

    made minor changes based on tips from stucox

--- feature-detects/css/checked.js
+++ feature-detects/css/checked.js
@@ -12,8 +12,8 @@
 !*/
 define(['Modernizr', 'createElement', 'testStyles'], function( Modernizr, createElement, testStyles ) {
   Modernizr.addTest('checked', function(){
-   return Modernizr.testStyles('#modernizr input {width:100px} #modernizr :checked {width:200px;display:block}', function(elem, rule){
-      var cb = document.createElement('input');
+   return testStyles('#modernizr input {width:100px} #modernizr :checked {width:200px;display:block}', function(elem, rule){
+      var cb = createElement('input');
       cb.setAttribute("type", "checkbox");
       cb.setAttribute("checked", "checked");
       elem.appendChild(cb);
