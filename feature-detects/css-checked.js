2c4ef9476208911e5bcd44c148bd91eacd6e57c6
Rich Archer <rich.archer@unboxedconsulting.com>
2013-03-26 21:27:48 +0200

    update :checked test to be more in line with similar

--- feature-detects/css-checked.js
+++ feature-detects/css-checked.js
@@ -1,10 +1,24 @@
+/*!
+{
+  "name": "CSS checked",
+  "property": ":checked",
+  "authors": ["richarcher"],
+  "tags": ["css"],
+  "notes": [{
+    "name": "Related Github Issue",
+    "href": "https://github.com/Modernizr/Modernizr/issues/845"
+  }]
+}
+!*/
 define(['Modernizr', 'createElement', 'testStyles'], function (Modernizr, createElement, testStyles) {
-  testStyles("#modernizr input {margin-left:0px;} #modernizr input:checked {margin-left: 20px;}", function (elem) {
-    var chx = createElement('input');
-    chx.type = "checkbox";
-    chx.checked = "checked";
+  Modernizr.addTest('csschecked', function () {
+    return testStyles("#modernizr input {margin-left:0px;} #modernizr input:checked {margin-left: 20px;}", function (elem) {
+      var chx = createElement('input');
+      chx.type = "checkbox";
+      chx.checked = "checked";
 
-    elem.appendChild(chx);
-    Modernizr.addTest('csschecked', elem.lastChild.offsetLeft >= 20);
-  }, 2);
+      elem.appendChild(chx);
+      return elem.lastChild.offsetLeft >= 20;
+    });
+  });
 });
