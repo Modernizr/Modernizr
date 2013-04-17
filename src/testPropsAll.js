528ecf5f6b4cede9ff4b3642f89e3ae26799e6a2
Stu Cox <stuart.cox@gmail.com>
2013-04-18 00:17:38 +0100

    Revert "Changed testAllPropse to take an options argument, rather than a handful of flags"
    This reverts commit 6d7136f7514077873311a056d3ae225a1392d7b9.

--- src/testPropsAll.js
+++ src/testPropsAll.js
@@ -5,16 +5,15 @@ define(['ModernizrProto', 'cssomPrefixes', 'is', 'testProps', 'domPrefixes', 'te
      *     the element including the non-vendor prefixed one, for forward-
      *     compatibility.
      */
-    function testPropsAll( prop, prefixed, elem, value, options ) {
+    function testPropsAll( prop, prefixed, elem, value, useValue ) {
 
         var ucProp = prop.charAt(0).toUpperCase() + prop.slice(1),
-            props = (prop + ' ' + cssomPrefixes.join(ucProp + ' ') + ucProp).split(' '),
-            values = is(value, 'undefined') ? undefined
-                   : options && options.prefixValues ? prefixes.join(value + '\n').split('\n').slice(0, -1) : [value];
+            props = (prop + ' ' + cssomPrefixes.join(ucProp + ' ') + ucProp).split(' ');
+            values = is(value, 'undefined') ? undefined : prefixes.join(value + '\n').split('\n').slice(0, -1);
 
         // did they call .prefixed('boxSizing') or are we just testing a prop?
         if(is(prefixed, "string") || is(prefixed, "undefined")) {
-            return testProps(props, prefixed, values, options && options.useValue);
+            return testProps(props, prefixed, values, useValue);
 
             // otherwise, they called .prefixed('requestAnimationFrame', window[, elem])
         } else {
