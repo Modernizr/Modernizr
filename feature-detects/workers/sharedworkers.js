ea17d406af4e45a1f4ae66b2e7d4fad25f45c160
Stu Cox <stuart.cox@gmail.com>
2013-05-22 18:23:01 +0100

    Added metadata to "workers" detects

--- feature-detects/workers/sharedworkers.js
+++ feature-detects/workers/sharedworkers.js
@@ -1,3 +1,20 @@
+/*!
+{
+  "name": "Shared Workers",
+  "property": "sharedworkers",
+  "caniuse" : "sharedworkers",
+  "tags": ["performance", "workers"],
+  "notes": [{
+    "name": "W3C Reference",
+    "href": "http://www.w3.org/TR/workers/"
+  }]
+}
+!*/
+/* DOC
+
+Detects support for the `SharedWorker` API from the Web Workers spec.
+
+*/
 define(['Modernizr'], function( Modernizr ) {
   Modernizr.addTest('sharedworkers', !!window.SharedWorker);
 });
