cee193c445307d5fdc4359c8ff0b76fc7e2a5cc9
Patrick Kettner <patrick@patrickkettner.com>
2013-05-25 19:57:15 -0700

    updated/fixes #696 - adds preload and loop checks for video and audio elements

---
+++ feature-detects/audio/loop.js
@@ -0,0 +1,10 @@
+/*!
+{
+  "name": "Audio Loop Attribute",
+  "property": "audioloop",
+  "tags": ["audio", "media"]
+}
+!*/
+define(['Modernizr', 'createElement'], function( Modernizr, createElement ) {
+    Modernizr.addTest('audioloop', 'loop' in createElement('audio'));
+}); 
