fe85d9d5c46b4efdadc7219d8336c872a46a6dc0
Stu Cox <stuart.cox@gmail.com>
2013-05-30 23:10:18 +0100

    Added metadata to Modernizr.framed

--- feature-detects/window/framed.js
+++ feature-detects/window/framed.js
@@ -1,5 +1,16 @@
+/*!
+{
+  "name": "Framed window",
+  "property": "framed",
+  "tags": ["window"]
+}
+!*/
+/* DOC
+
+Tests if page is iframed.
+
+*/
 define(['Modernizr'], function( Modernizr ) {
-  // tests if page is iframed
   // github.com/Modernizr/Modernizr/issues/242
 
   Modernizr.addTest('framed', window.location != top.location);
