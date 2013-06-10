e377c2cb062687aeb9e65ac67674da9f8f2f2806
Alex Sexton <alexsexton@gmail.com>
2013-06-10 18:58:37 -0500

    Fixing bad metadata structure

--- feature-detects/websockets.js
+++ feature-detects/websockets.js
@@ -4,8 +4,13 @@
   "property": "websockets",
   "authors": ["Phread [fearphage]", "Mike Sherov [mikesherov]", "Burak Yigit Kaya [BYK]"],
   "caniuse": "websockets",
-  "warnings": "This test will reject any old version of WebSockets even if it is not prefixed such as in Safari 5.1",
-  "notes": "More info about the CLOSING state can be found at the spec: http://www.w3.org/TR/websockets/#the-websocket-interface",
+  "warnings": [
+    "This test will reject any old version of WebSockets even if it is not prefixed such as in Safari 5.1"
+  ],
+  "notes": [{
+    "name": "CLOSING State and Spec",
+    "href": "http://www.w3.org/TR/websockets/#the-websocket-interface"
+  }],
   "tags": ["html5"]
 }
 !*/
