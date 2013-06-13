cb03dbf8f5b479308f52f23d41843db611155bb9
Richard Herrera <rich@doctyper.com>
2013-06-13 10:35:02 -0700

    Use "matchdep" to avoid requiring dev dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -184,14 +184,7 @@ module.exports = function( grunt ) {
     }
   });
   // Load required contrib packages
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  require('matchdep').filterAll('grunt-*').forEach(grunt.loadNpmTasks);
 
   // Strip define fn
   grunt.registerMultiTask('stripdefine', "Strip define call from dist file", function() {
