b10362e4b29ec87ad4b4c0c111430fe7794bbff0
Patrick Kettner <patrick@patrickkettner.com>
2013-06-16 11:23:32 -0400

    add navigator.standalone test - fixes #173

---
+++ feature-detects/standalone.js
@@ -0,0 +1,13 @@
+/*!
+{
+  "name": "navigator.standalone",
+  "property": "standalone",
+  "notes": [{
+    "name": "Apple Documentation"
+    "href": "http://developer.apple.com/library/safari/#documentation/appleapplications/reference/SafariHTMLRef/Articles/MetaTags.html"
+  }]
+}
+!*/
+define(['Modernizr'], function( Modernizr) {
+  Modernizr.addTest('standalone', !!window.navigator.standalone);
+});
