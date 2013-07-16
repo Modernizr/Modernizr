dc4fe55f117281ef53eb6b7bfe73765aa9061033
Asmod4n <hendrik-github@beskow.de>
2013-07-16 14:40:18 +0200

    Checks for Desktop Notifications in modern Browsers.

--- feature-detects/notification.js
+++ feature-detects/notification.js
@@ -3,22 +3,22 @@
   "name": "Notifications",
   "property": "notification",
   "caniuse": "notifications",
-  "authors": ["Theodoor van Donge"],
+  "authors": ["Theodoor van Donge", "Hendrik Beskow"],
   "notes": [{
     "name": "HTML5 Rocks tutorial",
     "href": "http://www.html5rocks.com/en/tutorials/notifications/quick/"
   },{
     "name": "W3C spec",
-    "href": "http://dev.w3.org/2006/webapi/WebNotifications/publish/Notifications.html#idl-if-Notification"
+    "href": "www.w3.org/TR/notifications/"
   }],
-  "polyfills": ["notificationjs"]
+  "polyfills": ["desktop-notify"]
 }
 !*/
 /* DOC
 
-Detects support for the Notifications API, for notifying the user of events regardless of which tab has focus.
+Detects support for the Notifications API
 
 */
-define(['Modernizr', 'prefixed'], function( Modernizr, prefixed ) {
-  Modernizr.addTest('notification', !!prefixed('Notifications', window));
+define(['Modernizr'], function( Modernizr ) {
+  Modernizr.addTest('notification', (function () {var isSupported = false;try {isSupported = !!(/* Safari, Chrome */window.Notification || /* Chrome & ff-html5notifications plugin */window.webkitNotifications || /* Firefox Mobile */navigator.mozNotification || /* IE9+ */(window.external && window.external.msIsSiteMode() !== undefined));} catch (e) {} return isSupported;}()));
 });
