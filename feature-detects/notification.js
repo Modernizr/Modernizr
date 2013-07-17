558c37c6fcb7fea84cb79cd347840f21c0e1557f
Asmod4n <hendrik-github@beskow.de>
2013-07-17 10:25:10 +0200

    Update notification.js

--- feature-detects/notification.js
+++ feature-detects/notification.js
@@ -20,5 +20,17 @@ Detects support for the Notifications API
 
 */
 define(['Modernizr'], function( Modernizr ) {
-  Modernizr.addTest('notification', (function () {var isSupported = false;try {isSupported = !!(/* Safari, Chrome */window.Notification || /* Chrome & ff-html5notifications plugin */window.webkitNotifications || /* Firefox Mobile */navigator.mozNotification || /* IE9+ */(window.external && window.external.msIsSiteMode() !== undefined));} catch (e) {} return isSupported;}()));
+  Modernizr.addTest('notification', function () {
+      var isSupported = false;
+      try {
+        isSupported = !!(
+           /* Safari, Chrome */window.Notification
+        || /* Chrome & ff-html5notifications plugin */window.webkitNotifications
+        || /* Firefox Mobile */navigator.mozNotification
+        || /* IE9+ */(window.external && window.external.msIsSiteMode() !== undefined)
+        );
+      } catch (e) {}
+      
+      return isSupported;
+    }
 });
