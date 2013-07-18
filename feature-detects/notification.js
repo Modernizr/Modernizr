7542def23929d56470cb93939d7be955d140f04d
Asmod4n <hendrik-github@beskow.de>
2013-07-18 05:46:41 +0200

    Check for useable native Implementations

--- feature-detects/notification.js
+++ feature-detects/notification.js
@@ -23,10 +23,7 @@ define(['Modernizr'], function(Modernizr) {
   Modernizr.addTest('notification', function() {
       var isSupported = false;
       try {
-        isSupported = !!(/* Safari, Chrome, Firefox */window.Notification ||
-        /* Chrome & ff-html5notifications plugin */window.webkitNotifications ||
-        /* Firefox Mobile */navigator.mozNotification ||
-        /* IE9+ */(window.external && window.external.msIsSiteMode() !== undefined));
+        isSupported = !!(/* Safari, Firefox */window.Notification && window.Notification.permission && window.Notification.requestPermission);
       } catch(e) {}
       return isSupported;
   });
