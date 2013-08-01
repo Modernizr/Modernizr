a8d20c503ef4973ded147a7f61070013ce40d36b
Stu Cox <stuart.cox@gmail.com>
2013-08-01 10:33:08 +0100

    Finishing my sentence…

--- src/testAllProps.js
+++ src/testAllProps.js
@@ -5,7 +5,8 @@ define(['ModernizrProto', 'testPropsAll'], function( ModernizrProto, testPropsAl
    * which case testAllProps will only return true if the browser supports that
    * value for the named property; this latter case will use native detection
    * (via window.CSS.supports) if available. A boolean can be passed as a 3rd
-   * parameter to
+   * parameter to skip the value check when native detection isn't available,
+   * to improve performance when simply testing for support of a property.
    *
    * @param prop - String naming the property to test
    * @param value - [optional] String of the value to test
