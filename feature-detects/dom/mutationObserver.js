ee0c236080098fcc15f0baffc823faf1a86c2717
Karel Sedlàček <k@ksdlck.com>
2013-08-06 15:12:11 +0200

    add DOC and note to DOM4 MutationObserver feature detect

--- feature-detects/dom/mutationObserver.js
+++ feature-detects/dom/mutationObserver.js
@@ -4,9 +4,18 @@
   "property": "mutationobserver",
   "caniuse": "mutationobserver",
   "tags": ["dom"],
-  "authors": ["Karel Sedláček"]
+  "authors": ["Karel Sedláček"],
+  "notes": [{
+    "name": "MDN documentation",
+    "href": "https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver"
+  }]
 }
 !*/
+/* DOC
+
+Determines if DOM4 MutationObserver support is available.
+
+*/
 define(['Modernizr'], function( Modernizr ) {
   Modernizr.addTest('mutationobserver',
     !!window.MutationObserver || !!window.WebKitMutationObserver);
