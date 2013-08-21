89727242dcea1a7ede6aab92d10f7088e40b912b
remmons <reed.emmons@gmail.com>
2013-08-21 10:27:42 -0400

    Adding print test

---
+++ feature-detects/print.js
@@ -0,0 +1,20 @@
+/*!
+{
+  "name": "Print API",
+  "property": "print",
+  "caniuse": "print",
+  "tags": ["print"],
+  "notes": [{
+    "name": "MDN",
+    "href": "https://developer.mozilla.org/en-US/docs/Web/API/window.print"
+  }]
+}
+!*/
+/* DOC
+
+Detects support for the Printing.
+
+*/
+define(['Modernizr'], function( Modernizr ) {
+  Modernizr.addTest('print', 'print' in window);
+});
