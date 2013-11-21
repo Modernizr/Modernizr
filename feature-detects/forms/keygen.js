e5e938d4845e6c1482abdee5da79955e0c11c6ba
micole <micole.3@gmail.com>
2013-11-21 14:54:33 -0500

    moving branches

--- feature-detects/forms/keygen.js
+++ feature-detects/forms/keygen.js
@@ -8,10 +8,26 @@
 !*/
 define(['Modernizr'], function( Modernizr ) {
   Modernizr.addTest('keygen', function(){
-    var div = document.createElement('div');
+    /*var form = createElement('form');
+    form.addEventListener('submit', function(e) {
+      if(!window.opera){
+        e.preventDefault();
+      }
+      e.stopPropagation();
+    }, false);
+
+    form.innerHTML = '<keygen /><button></button>';*/
+
+
+    /*var div = document.createElement('div');
     div.innerHTML = '<keygen />';
     var keygen = div.childNodes[0];
-    return keygen.type === 'keygen' || "_moz-type" in keygen.attributes;
+    ///return keygen.isSupported();
+    //var keygen2 = keygen.cloneNode();
+    //return keygen2.type === 'keygen' || "_moz-type" in keygen2.attributes;*/
+
+    var keygen = document.createElement('keygen');
+    return document.window.keygen.keytype === "";
   });
 });
 
