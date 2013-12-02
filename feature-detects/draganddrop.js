a77b69e8cb155d5d43fe57f34ebbede3f781d8d7
Patrick Kettner <patrickkettner@gmail.com>
2013-12-01 21:14:38 -0500

    add bug notes about dragndrop on mobile

--- feature-detects/draganddrop.js
+++ feature-detects/draganddrop.js
@@ -3,6 +3,7 @@
   "name": "Drag & Drop",
   "property": "draganddrop",
   "caniuse": "dragndrop",
+  "knownBugs": ["Mobile browsers like Android, iOS < 6, and Firefox OS technically support the APIs, but don't expose it to the end user, resulting in a false positive."],
   "notes": [{
     "name": "W3C spec",
     "href": "http://www.w3.org/TR/2010/WD-html5-20101019/dnd.html"
