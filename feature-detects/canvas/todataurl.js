353ca65ce239c8cf883a590b7ff895cacd2364a1
jimmywarting <jimmy@warting.se>
2013-12-20 16:04:14 +0100

    execute faster, less code

--- feature-detects/canvas/todataurl.js
+++ feature-detects/canvas/todataurl.js
@@ -12,16 +12,15 @@
 !*/
 define(['Modernizr', 'createElement', 'test/canvas'], function( Modernizr, createElement ) {
 
-  var canvas = createElement('canvas');
-
-  Modernizr.addTest('todataurljpeg', function() {
-    return !!Modernizr.canvas && canvas.toDataURL('image/jpeg').indexOf('data:image/jpeg') === 0;
-  });
-  Modernizr.addTest('todataurlpng', function() {
-    return !!Modernizr.canvas && canvas.toDataURL('image/png').indexOf('data:image/png') === 0;
-  });
-  Modernizr.addTest('todataurlwebp', function() {
-    return !!Modernizr.canvas && canvas.toDataURL('image/webp').indexOf('data:image/webp') === 0;
-  });
+    var canvas = createElement('canvas'),
+        type = ["jpeg", "png", "webp"],
+        i = 0;
+    
+    // Speeds up the code http://jsperf.com/test-todataurl-speed
+    canvas.width = canvas.height = 1;
+    
+    for(;i<3;i++){
+        Modernizr.addTest('todataurl' + type[i], !!Modernizr.canvas && canvas.toDataURL('image/' + type[i]).indexOf('data:image/' + type[i]) === 0)
+    }
 
 });
