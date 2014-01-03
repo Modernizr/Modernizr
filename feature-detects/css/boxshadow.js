21447ae44cf3bfd37091422b6e153186d39a486f
Patrick Kettner <patrickkettner@gmail.com>
2014-01-02 23:21:22 -0500

    add note about the silk browser having a false positive on box-shadow

--- feature-detects/css/boxshadow.js
+++ feature-detects/css/boxshadow.js
@@ -4,7 +4,10 @@
   "property": "boxshadow",
   "caniuse": "css-boxshadow",
   "tags": ["css"],
-  "knownBugs": ["WebOS false positives on this test."]
+  "knownBugs": [
+    "WebOS false positives on this test.",
+    "The Kindle Silk browser false positives"
+  ]
 }
 !*/
 define(['Modernizr', 'testAllProps'], function( Modernizr, testAllProps ) {
