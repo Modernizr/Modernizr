99a7035f8233bc9e6583d5673d066635d61afc43
Rob Wierzbowski <hello@robwierzbowski.com>
2014-01-12 17:14:13 -0500

    Improve test coverage for new build system

--- test/api/build.js
+++ test/api/build.js
@@ -3,57 +3,68 @@
 var fs = require('fs');
 var path = require('path');
 var cwd = process.cwd();
-
-var config = path.join(cwd, 'lib', 'config-all.json');
-var _cache;
-
-exports.setUp = function (callback) {
-  _cache = fs.readFileSync(config, 'utf8');
-  callback();
-};
+var modernizr = require(path.join(cwd, 'lib/modernizr'));
 
 exports.build = function (test) {
-  test.expect(3);
-  var modernizr = require(path.join(cwd, 'lib', 'cli'));
+  test.expect(1);
+  test.equal(typeof modernizr.build, 'function', 'Should export build function');
+  test.done();
+};
 
-  test.ok(modernizr.build, 'should export build function');
-  test.equal(typeof modernizr.build, 'function', 'modernizr.build should be a function');
+exports.buildAll = function (test) {
+  var config = require(path.join(cwd, 'lib', 'config-all.json'));
 
-  modernizr.build({
-    'verbose': false
-  }, function () {
-    test.ok(true, 'should finish build without errors');
-    test.done();
+  test.expect(4);
+  modernizr.build(config, {
+    verbose: false,
+    dest: false,
+    callback: function (output) {
+      var missingFeatures = false;
+
+      config['feature-detects'].forEach(function (feature) {
+        feature = feature.match(/([^\/]+$)/)[0];
+        if (output.indexOf(feature) === -1) {
+          missingFeatures = true;
+        }
+      });
+
+      test.ok(true, 'Should finish build without errors');
+      test.ok(missingFeatures, 'Should find all feature-detects given in config-all.json');
+      test.notEqual(output.indexOf('function shivPrint'), -1, 'Should find the print shiv');
+      test.notEqual(output.indexOf('// yepnope.js'), -1, 'Should find yepnope');
+      test.done();
+    }
   });
 };
 
 exports.buildCustom = function (test) {
-  test.expect(6);
-  var modernizr = require(path.join(cwd, 'lib', 'cli'));
-
-  test.ok(modernizr.build, 'should export build function');
-  test.equal(typeof modernizr.build, 'function', 'modernizr.build should be a function');
-
-  modernizr.build({
+  var config = {
+    'classPrefix': 'modz',
+    'options': [
+      'setClasses',
+      'addTest',
+      'testProp',
+      'fnBind'
+    ],
     'feature-detects': [
       'test/css/boxsizing',
       'test/dom/classlist'
-    ],
-    'verbose': false
-  }, function () {
-    test.ok(true, 'should finish build without errors');
-    var file = fs.readFileSync(path.join(cwd, 'dist', 'modernizr-build.js'), 'utf8');
+    ]
+  };
 
-    test.notEqual(file.indexOf('boxsizing'), -1, 'Should find test for boxsizing');
-    test.notEqual(file.indexOf('classlist'), -1, 'Should find test for classlist');
-
-    test.equal(file.indexOf('cssanimations'), -1, 'Should not find test for cssanimations');
-
-    test.done();
+  test.expect(7);
+  modernizr.build(config, {
+    verbose: false,
+    dest: false,
+    callback: function (output) {
+      test.ok(true, 'should finish build without errors');
+      test.notEqual(output.indexOf('classPrefix: \'modz\''), -1, 'Should set custom class');
+      test.equal(output.indexOf('function shivPrint'), -1, 'Should not find the print shiv');
+      test.equal(output.indexOf('// yepnope.js'), -1, 'Should not find yepnope');
+      test.notEqual(output.indexOf('boxsizing'), -1, 'Should find test for boxsizing');
+      test.notEqual(output.indexOf('classlist'), -1, 'Should find test for classlist');
+      test.equal(output.indexOf('cssanimations'), -1, 'Should not find test for cssanimations');
+      test.done();
+    }
   });
-};
-
-exports.tearDown = function (callback) {
-  fs.writeFileSync(config, _cache);
-  callback();
 };
