ee833bcdcb5d8bfef6157d45199432e248633224
Chris Coyier <chriscoyier@gmail.com>
2014-02-03 14:37:30 -0800

    fixups

--- feature-detects/svg/asimg.js
+++ feature-detects/svg/asimg.js
@@ -4,22 +4,22 @@
   "property": "svgasimg",
   "caniuse" : "svg-img",
   "tags": ["svg"],
-  "authors": ["Stu Cox", "Chris Coyier"]
+  "authors": ["Chris Coyier"]
   "notes": [{
     "name": "HTML5 Spec",
     "href": "http://www.w3.org/TR/html5/embedded-content-0.html#the-img-element"
   }]
 }
 !*/
-define(['Modernizr', 'addTest'], function( Modernizr, addTest ) {
+define(['Modernizr'], function( Modernizr, addTest ) {
 
   // Orignal Async test by Stu Cox
   // https://gist.github.com/chriscoyier/8774501
 
-  // Now a Sync test based on indential results as shown here
+  // Now a Sync test based on good results here
   // http://codepen.io/chriscoyier/pen/bADFx
   
   Modernizr.addTest('svgasimg', function () {
-    return !!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
-  });  
+    return document.implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#Image', '1.1');
+  });
 });
