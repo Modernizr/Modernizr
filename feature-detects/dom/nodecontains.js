54fb19304c4e6c2a2d16cf167acf09fe22a0d608
aaronk6 <aaronk6@users.noreply.github.com>
2014-02-28 14:27:45 +0100

    Improved meta data for Node.contains

--- feature-detects/dom/nodecontains.js
+++ feature-detects/dom/nodecontains.js
@@ -1,15 +1,18 @@
 /*!
 {
-  "name": "contains",
-  "property": "contains",
+  "name": "Node.contains",
+  "property": "nodecontains",
   "tags": ["dom"],
   "authors": ["aaronk6"],
   "notes": [{
     "name": "MDN Docs",
     "href": "https://developer.mozilla.org/en-US/docs/Web/API/Node.contains"
+  },{
+    "name": "W3C DOM4",
+    "href": "http://www.w3.org/TR/domcore/#dom-node-contains"
   }]
 }
 !*/
 define(['Modernizr', 'docElement'], function( Modernizr, docElement ) {
-  Modernizr.addTest('contains', 'contains' in docElement);
+  Modernizr.addTest('nodecontains', 'contains' in docElement);
 });
