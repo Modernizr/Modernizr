5e4490e00cd6da0f9a44d6ddbba372fd259c4643
aaronk6 <aaronk6@users.noreply.github.com>
2014-03-03 08:55:49 +0100

    Added /* DOC */ block for nodecontains detect

--- feature-detects/dom/nodecontains.js
+++ feature-detects/dom/nodecontains.js
@@ -13,6 +13,14 @@
   }]
 }
 !*/
+/* DOC
+
+Detect support for the `contains` attribute of a DOM node.
+
+You might want to test this since Mozilla implemented this feature quite late.  
+(Firefox 9.0/Gecko 9.0 which was released in late 2011)
+
+*/
 define(['Modernizr', 'docElement'], function( Modernizr, docElement ) {
   Modernizr.addTest('nodecontains', 'contains' in docElement);
 });
