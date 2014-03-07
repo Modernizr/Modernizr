27a7c67df6f20dcb8e0115ad8c5680e536cf5dfe
Ryan Seddon <seddon.ryan@gmail.com>
2014-03-07 11:20:12 +1100

    Updating oooold jquery version to latest. See if that helps with odds errors in automation.

--- test/js/unit.js
+++ test/js/unit.js
@@ -62,7 +62,7 @@ test('bind is implemented', function() {
   ok('prototype' in F, '"prototype" in F');
 
   // The object passed to bind as 'this' must be callable.
-  raises(function(){
+  throws(function(){
     Function.bind.call(undefined);
   });
 
