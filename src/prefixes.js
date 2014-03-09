c06f15600f2b7353a4c0f77d6eaa9242f2fd7122
Patrick Kettner <patrickkettner@gmail.com>
2014-03-09 04:17:44 -0400

    update Modernizr._prefixes to only return the supported prefixes

--- src/prefixes.js
+++ src/prefixes.js
@@ -1,6 +1,29 @@
-define(['ModernizrProto'], function( ModernizrProto ) {
+define(['ModernizrProto', 'docElement'], function( ModernizrProto, docElement) {
   // List of property values to set for css tests. See ticket #21
-  var prefixes = (ModernizrProto._config.usePrefixes ? ' -webkit- -moz- -o- -ms- '.split(' ') : []);
+  var prefixes = [];
+  var prefix;
+  var styles;
+
+  if (ModernizrProto._config.usePrefixes) {
+    // Empty string for prefixless support
+    prefixes.push('');
+
+    // old ie does not have a getComputedStyle, but does offer the MS specific
+    // 'currentStyle', so if it exists, we are -ms-
+    if ('currentStyle' in docElement) {
+      prefix = '-ms-';
+    } else {
+      styles = window.getComputedStyle(docElement, '');
+      prefix = ([].slice
+                .call(styles)
+                .join('')
+                .match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['-o-'])
+               )[0];
+    }
+
+    ModernizrProto._prefix = prefix;
+    prefixes.push(prefix);
+  }
 
   // expose these for the plugin API. Look in the source for how to join() them against your input
   ModernizrProto._prefixes = prefixes;
