cf028dd87d11e788a1f74a5dd3cf6da9c17a151a
Scott Vinkle <svinkle@gmail.com>
2014-03-18 22:24:49 -0400

    Update video.js
    Answering @darcy's twitter challenge.

--- feature-detects/video.js
+++ feature-detects/video.js
@@ -47,7 +47,7 @@ define(['Modernizr', 'createElement'], function( Modernizr, createElement ) {
         bool.ogg = elem.canPlayType('video/ogg; codecs="theora"').replace(/^no$/,'');
 
         // Without QuickTime, this value will be `undefined`. github.com/Modernizr/Modernizr/issues/546
-        bool.h264 = elem.canPlayType('video/mp4; codecs="avc1.42E01E"') .replace(/^no$/,'');
+        bool.h264 = elem.canPlayType('video/mp4; codecs="avc1.42E01E"').replace(/^no$/,'');
 
         bool.webm = elem.canPlayType('video/webm; codecs="vp8, vorbis"').replace(/^no$/,'');
 
