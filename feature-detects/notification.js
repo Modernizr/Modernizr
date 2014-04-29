6ee02df18af15a1cc8487209c8da78537bb5be25
Patrick Kettner <patrickkettner@gmail.com>
2014-04-29 00:54:12 -0400

    backport of 2e0a658718 / #1015

--- feature-detects/notification.js
+++ feature-detects/notification.js
@@ -7,4 +7,4 @@
 // window.Notification only exist in the draft specs 
 //	http://dev.w3.org/2006/webapi/WebNotifications/publish/Notifications.html#idl-if-Notification
 
-Modernizr.addTest('notification', !!Modernizr.prefixed('Notifications', window));
+Modernizr.addTest('notification', 'Notification' in window && 'permission' in window.Notification && 'requestPermission' in window.Notification);
