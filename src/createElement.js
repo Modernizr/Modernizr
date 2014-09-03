4e42ae4ef40be87e2590afa0a710b08a848df0de
Vladislav Ponomarev <ponomarevvlad@gmail.com>
2014-09-03 16:23:05 +0200

    Fix for document.createElement invocation in IE7

--- src/createElement.js
+++ src/createElement.js
@@ -1,5 +1,11 @@
 define(function() {
   var createElement = function() {
+    if (typeof document.createElement !== 'function') {
+      var oldCreateElement = document.createElement;
+      document.createElement = function (tagName) {
+        return oldCreateElement(tagName);
+      };
+    }
     return document.createElement.apply(document, arguments);
   };
   return createElement;
