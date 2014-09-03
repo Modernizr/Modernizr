3e25189fb055f6872ed8b7a5fa35b1a7d11c9446
Vladislav Ponomarev <ponomarevvlad@gmail.com>
2014-09-03 23:57:34 +0200

    Fixing comments to pull request

--- src/createElement.js
+++ src/createElement.js
@@ -1,12 +1,12 @@
 define(function() {
   var createElement = function() {
     if (typeof document.createElement !== 'function') {
-      var oldCreateElement = document.createElement;
-      document.createElement = function (tagName) {
-        return oldCreateElement(tagName);
-      };
+      // This is the case in IE7, where the type of createElement is "object".
+      // For this reason, we cannot call apply() as Object is not a Function.
+      return document.createElement(arguments[0]);
+    } else {
+      return document.createElement.apply(document, arguments);
     }
-    return document.createElement.apply(document, arguments);
   };
   return createElement;
 });
