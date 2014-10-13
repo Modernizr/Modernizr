8fc5ebd3dcea7783c37d6651ccee57847f350fbe
Oliver Jean Eifler <olli.eifler@gmail.com>
2014-10-13 19:25:27 +0200

    Fix: cssToDOM for IE <= 7
    if name is not a string return emty string

--- src/cssToDOM.js
+++ src/cssToDOM.js
@@ -2,6 +2,9 @@ define(function() {
   // Helper function for converting kebab-case to camelCase,
   // e.g. box-sizing -> boxSizing
   function cssToDOM( name ) {
+    //old ie hack
+    if (typeof name !== "string")
+        return "";
     return name.replace(/([a-z])-([a-z])/g, function(str, m1, m2) {
       return m1 + m2.toUpperCase();
     }).replace(/^-/, '');
