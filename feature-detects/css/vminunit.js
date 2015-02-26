b8ea91ca4fc7775123738a7701e40549109a3400
Patrick Kettner <patrickkettner@gmail.com>
2015-02-26 00:22:06 -0500

    fix vmin and vmax tests for windows browsers

--- feature-detects/css/vminunit.js
+++ feature-detects/css/vminunit.js
@@ -15,12 +15,18 @@
 }
 !*/
 define(['Modernizr', 'docElement', 'testStyles'], function( Modernizr, docElement, testStyles ) {
-  testStyles('#modernizr { width: 50vmin; }', function( elem ) {
+  testStyles('#modernizr1{width: 50vmin}#modernizr2{width:50px;height:50px;overflow:scroll}', function() {
+    var elem = document.getElementById('modernizr1');
+    var scroller = document.getElementById('modernizr2');
+    var scrollbarWidth = parseInt((scroller.offsetWidth - scroller.clientWidth) / 2, 10);
+
     var one_vw = docElement.clientWidth/100;
     var one_vh = docElement.clientHeight/100;
+    var expectedWidth = parseInt(Math.min(one_vw, one_vh) * 50, 10);
     var compWidth = parseInt((window.getComputedStyle ?
                           getComputedStyle(elem, null) :
                           elem.currentStyle)['width'],10);
-    Modernizr.addTest('cssvminunit', parseInt(Math.min(one_vw, one_vh)*50,10) == compWidth );
-  });
+
+    Modernizr.addTest('cssvminunit', expectedWidth === compWidth || expectedWidth === compWidth - scrollbarWidth);
+  }, 2);
 });
