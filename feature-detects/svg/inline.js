71e9e73e5e203872b961a9562e02e4785a680e67
Elton Mesquita <callmeelton@gmail.com>
2015-03-11 18:12:49 -0300

    Fix issue #1552
    Add known bug to inline SVG test
    
    Fixes #1553

--- feature-detects/svg/inline.js
+++ feature-detects/svg/inline.js
@@ -7,17 +7,21 @@
   "notes": [{
     "name": "Test page",
     "href": "http://paulirish.com/demo/inline-svg"
+  }, {
+    "name": "Test page and results",
+    "href": "http://codepen.io/eltonmesquita/full/GgXbvo/"
   }],
-  "polyfills": ["inline-svg-polyfill"]
+  "polyfills": ["inline-svg-polyfill"],
+  "knownBugs": ["False negative on some Chromia browsers."]
 }
 !*/
 /* DOC
 Detects support for inline SVG in HTML (not within XHTML).
 */
-define(['Modernizr', 'createElement'], function(Modernizr, createElement) {
+define(['Modernizr', 'createElement'], function( Modernizr, createElement ) {
   Modernizr.addTest('inlinesvg', function() {
     var div = createElement('div');
     div.innerHTML = '<svg/>';
-    return (div.firstChild && div.firstChild.namespaceURI) == 'http://www.w3.org/2000/svg';
+    return (typeof SVGRect != 'undefined' && div.firstChild && div.firstChild.namespaceURI) == 'http://www.w3.org/2000/svg';
   });
 });
