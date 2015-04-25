af454058b9364c03ec78dd10171bb81854fc4e30
Patrick Kettner <patrickkettner@gmail.com>
2015-04-25 00:30:51 -0400

    remove outdated firefox bug from gamepad docs
    fixes #1561 [ci skip]

--- feature-detects/gamepad.js
+++ feature-detects/gamepad.js
@@ -19,10 +19,6 @@
 Detects support for the Gamepad API, for access to gamepads and controllers.
 */
 define(['Modernizr', 'prefixed'], function(Modernizr, prefixed) {
-  // FF has Gamepad API support only in special builds, but not in any release (even behind a flag)
-  // Their current implementation has no way to feature detect, only events to bind to, but a patch
-  // will bring them up to date with the spec when it lands (and they'll pass this test)
-  //   https://bugzilla.mozilla.org/show_bug.cgi?id=690935
 
   Modernizr.addTest('gamepads', !!prefixed('getGamepads', navigator));
 });
