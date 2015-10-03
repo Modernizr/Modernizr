23ff3942a02b8682cc30864cb178199637e4190f
Christian Ulbrich (Zalari UG haftungsbeschränkt) <christian.ulbrich@zalari.de>
2015-10-03 03:26:16 +0200

    add WindowBase64 API detection (window.atob && window.btoa)

---
+++ feature-detects/window/atob-btoa.js
@@ -0,0 +1,25 @@
+/*!
+{
+  "name": "Base 64 encoding/decoding",
+  "property": ["atob-btoa"],
+  "caniuse" : "atob-btoa",
+  "tags": ["atob", "base64", "WindowBase64", "btoa"],
+  "authors": ["Christian Ulbrich"],
+  "notes": [{
+    "name": "WindowBase64",
+    "href": "http://www.w3.org/TR/html5/webappapis.html#windowbase64"
+  }, {
+    "name": "MDN documentation",
+    "href": "https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/atob"
+  }],
+  "polyfills": ["base64js"]
+}
+!*/
+/* DOC
+
+Detects support for WindowBase64 API (window.atob && window.btoa).
+
+*/
+define(['Modernizr'], function(Modernizr) {
+  Modernizr.addTest('atob-btoa', 'atob' in window && 'btoa' in window);
+});
