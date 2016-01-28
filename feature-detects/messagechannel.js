6111b220e149d4b5aa239269c0303dfaea660950
Konga Raju <kongaraju@gmail.com>
2016-01-28 20:32:57 +0530

    Create messagechannel.js

---
+++ feature-detects/messagechannel.js
@@ -0,0 +1,22 @@
+/*!
+{
+  "name": "Message Channel",
+  "property": "MessageChannel",
+  "caniuse" : "MessageChannel",
+  "tags": ["performance", "messagechannel"],
+  "notes": [{
+    "name": "W3C Reference",
+    "href": "https://www.w3.org/TR/2011/WD-webmessaging-20110317/#message-channels"
+  }, {
+    "name": "MDN documentation",
+    "href": "https://developer.mozilla.org/en-US/docs/Web/API/Channel_Messaging_API/Using_channel_messaging"
+  }],
+  "polyfills": ["messagechannel"]
+}
+!*/
+/* DOC
+Detects support for  `Message Channel` API from the Channel Messaging spec. Message Channels  provide a simple way for communication between threads.
+*/
+define(['Modernizr'], function(Modernizr) {
+  Modernizr.addTest('messagechannel', 'MessageChannel' in window);
+});
