f9ddfa2e008f291ed507f505e7c848062c509969
Paul Irish <paul.irish@gmail.com>
2016-08-03 10:01:20 -0700

    Flip logic for clarity.

--- feature-detects/css/transformstylepreserve3d.js
+++ feature-detects/css/transformstylepreserve3d.js
@@ -20,23 +20,25 @@ define(['Modernizr', 'createElement', 'docElement'], function(Modernizr, createE
   Modernizr.addTest('preserve3d', function() {
     var outerAnchor, innerAnchor;
     var CSS = window.CSS;
-    var result = true;
+    var result = false;
 
-    if (!CSS || !CSS.supports || !CSS.supports('(transform-style: preserve-3d)')) {
-      outerAnchor = createElement('a');
-      innerAnchor = createElement('a');
-
-      outerAnchor.style.cssText = 'display: block; transform-style: preserve-3d; transform-origin: right; transform: rotateY(40deg);';
-      innerAnchor.style.cssText = 'display: block; width: 9px; height: 1px; background: #000; transform-origin: right; transform: rotateY(40deg);';
-
-      outerAnchor.appendChild(innerAnchor);
-      docElement.appendChild(outerAnchor);
-
-      result = innerAnchor.getBoundingClientRect();
-      docElement.removeChild(outerAnchor);
-
-      result = result.width && result.width < 4;
+    if (CSS && CSS.supports && CSS.supports('(transform-style: preserve-3d)')) {
+      return true;
     }
+
+    outerAnchor = createElement('a');
+    innerAnchor = createElement('a');
+
+    outerAnchor.style.cssText = 'display: block; transform-style: preserve-3d; transform-origin: right; transform: rotateY(40deg);';
+    innerAnchor.style.cssText = 'display: block; width: 9px; height: 1px; background: #000; transform-origin: right; transform: rotateY(40deg);';
+
+    outerAnchor.appendChild(innerAnchor);
+    docElement.appendChild(outerAnchor);
+
+    result = innerAnchor.getBoundingClientRect();
+    docElement.removeChild(outerAnchor);
+
+    result = result.width && result.width < 4;
     return result;
   });
 });
