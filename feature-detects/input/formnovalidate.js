2dff60d5176bcc8339a5f46e7a69283dd409d871
Andre <a.ricardoaccioly@gmail.com>
2017-02-22 16:47:13 -0300

    Feature detection for formNoValidate attribute on inputs.

---
+++ feature-detects/input/formnovalidate.js
@@ -0,0 +1,23 @@
+/*!
+{
+  "name": "input formnovalidate",
+  "property": "inputformnovalidate",
+  "aliases": ["input-formnovalidate"],
+  "notes": [{
+    "name": "WHATWG Spec",
+    "href": "https://html.spec.whatwg.org/multipage/forms.html#attr-fs-formnovalidate"
+  }, {
+    "name": "Wufoo demo",
+    "href": "https://www.wufoo.com/html5/attributes/12-formnovalidate.html"
+  }],
+  "polyfills": [
+    "html5formshim"
+  ]
+}
+!*/
+/* DOC
+Detect support for the formnovalidate attribute on form inputs, which overrides the form novalidate attribute
+*/
+define(['Modernizr', 'createElement'], function(Modernizr, createElement) {
+  Modernizr.addTest('inputformnovalidate', !!('formNoValidate' in createElement('input')), {aliases: ['input-formnovalidate']});
+});
