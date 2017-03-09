3f37a6a140a7b607e7591850b83a0dc3c3aef67d
Fabien Chaynes <fabien.chaynes@dimelo.com>
2017-03-09 16:37:01 +0100

    Improve emoji detection (Fix #1688) Fix false negatives caused by the emoji position.

--- feature-detects/emoji.js
+++ feature-detects/emoji.js
@@ -12,14 +12,13 @@ define(['Modernizr', 'createElement', 'test/canvastext'], function(Modernizr, cr
     if (!Modernizr.canvastext) {
       return false;
     }
-    var pixelRatio = window.devicePixelRatio || 1;
-    var offset = 12 * pixelRatio;
     var node = createElement('canvas');
     var ctx = node.getContext('2d');
     ctx.fillStyle = '#f00';
-    ctx.textBaseline = 'top';
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
     ctx.font = '32px Arial';
     ctx.fillText('\ud83d\udc28', 0, 0); // U+1F428 KOALA
-    return ctx.getImageData(offset, offset, 1, 1).data[0] !== 0;
+    return ctx.getImageData(0, 0, 1, 1).data[0] !== 0;
   });
 });
