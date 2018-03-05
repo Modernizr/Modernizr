a9369b031d05b90231b572e2796afba0334b21e6
Chris Lienert <rez@iinet.net.au>
2018-03-05 09:59:50 +0800

    Brought across @myakura's code changes to fix #1812 (#2250)

--- feature-detects/webrtc/getusermedia.js
+++ feature-detects/webrtc/getusermedia.js
@@ -4,14 +4,16 @@
   "property": "getusermedia",
   "caniuse": "stream",
   "tags": ["webrtc"],
-  "authors": ["Eric Bidelman"],
+  "authors": ["Eric Bidelman", "Masataka Yakura"],
   "notes": [{
     "name": "W3C Media Capture and Streams spec",
-    "href": "https://www.w3.org/TR/mediacapture-streams/"
-  }],
-  "polyfills": ["getusermedia"]
+    "href": "http://w3c.github.io/mediacapture-main/#dom-mediadevices-getusermedia"
+  }]
 }
 !*/
-define(['Modernizr', 'prefixed'], function(Modernizr, prefixed) {
-  Modernizr.addTest('getusermedia', !!prefixed('getUserMedia', navigator));
+/* DOC
+Detects support for the new Promise-based `getUserMedia` API.
+*/
+define(['Modernizr'], function (Modernizr) {
+  Modernizr.addTest('getUserMedia', 'mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices);
 });
