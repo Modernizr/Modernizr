7a49651713d1371351be2f9987a7a7b26749c6d8
Veeck <github@veeck.de>
2018-05-31 12:43:25 +0200

    Try out simpler test

--- feature-detects/css/fontdisplay.js
+++ feature-detects/css/fontdisplay.js
@@ -2,6 +2,7 @@
 {
   "name": "Font Display",
   "property": "fontdisplay",
+  "caniuse": "css-font-rendering-controls",
   "notes": [{
     "name": "W3C CSS Fonts Module Level 4",
     "href": "https://drafts.csswg.org/css-fonts-4/#font-display-desc"
@@ -14,17 +15,6 @@
 /* DOC
 Detects support for the `font-display` descriptor, which defines how font files are loaded and displayed by the browser.
 */
-define(['Modernizr'], function(Modernizr) {
-  Modernizr.addTest('fontdisplay', function() {
-    try {
-      var e = document.createElement('style');
-      e.textContent = '@font-face { font-display: swap; }';
-      document.documentElement.appendChild(e);
-      var isSupported = e.sheet.cssRules[0].cssText.indexOf('font-display') != -1;
-      e.remove();
-      return isSupported;
-    } catch (e) {
-      return false;
-    }
-  });
+define(['Modernizr'], function(Modernizr, testProp) {
+  Modernizr.addTest('font-display', testProp('font-display', 'swap'));
 });
