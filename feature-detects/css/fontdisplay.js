3be70e30d5c6bb26c7f404ac7c307aea51ed32ad
Veeck <github@veeck.de>
2018-06-06 10:46:03 +0200

    Detect font display (#2323)
    * Add Font Display detection
    
    * Try out simpler test
    
    * Fix camelcase
    
    * Final fix and author recognition

---
+++ feature-detects/css/fontdisplay.js
@@ -0,0 +1,21 @@
+/*!
+{
+  "name": "Font Display",
+  "property": "fontdisplay",
+  "authors": ["Patrick Kettner"],
+  "caniuse": "css-font-rendering-controls",
+  "notes": [{
+    "name": "W3C CSS Fonts Module Level 4",
+    "href": "https://drafts.csswg.org/css-fonts-4/#font-display-desc"
+  },{
+    "name": "`font-display` for the masses",
+    "href": "https://css-tricks.com/font-display-masses/"
+  }]
+}
+!*/
+/* DOC
+Detects support for the `font-display` descriptor, which defines how font files are loaded and displayed by the browser.
+*/
+define(['Modernizr'], function(Modernizr, testProp) {
+  Modernizr.addTest('fontDisplay', testProp('font-display'));
+});
