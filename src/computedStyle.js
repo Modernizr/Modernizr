d728e97483d651a8386e6d7a241dfbe7d938f3d5
Veeck <michael@veeck.de>
2018-07-28 18:37:11 +0200

    Add optional tag back to parameter

--- src/computedStyle.js
+++ src/computedStyle.js
@@ -7,7 +7,7 @@ define(function() {
    * @access private
    * @function computedStyle
    * @param {HTMLElement|SVGElement} elem - The element we want to find the computed styles of
-   * @param {string|null} pseudo - An optional pseudo element selector (e.g. :before), of null if none
+   * @param {string|null} [pseudo] - An optional pseudo element selector (e.g. :before), of null if none
    * @param {string} prop - A CSS property
    * @returns {CSSStyleDeclaration} the value of the specified CSS property
    */
