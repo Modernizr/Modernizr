04181cef052c8dd530513d766c0485008f25cd4a
Veeck <github@veeck.de>
2018-07-30 17:12:58 +0200

    More test around text decoration styling (#2327)
    * Feature detect for text-decoration styling
    
    * Take PR from @stefanpl and merge it with test for text-decoration-skip-ink

--- feature-detects/css/textdecoration.js
+++ feature-detects/css/textdecoration.js
@@ -1,10 +1,41 @@
 /*!
 {
   "name": "CSS textDecoration",
-  "property": "skipink",
-  "tags": ["css"]
+  "property": "textdecoration",
+  "caniuse": "text-decoration",
+  "tags": ["css"],
+  "notes": [{
+    "name" : "w3c explanation of individual text-decoration properties",
+    "href": "https://www.w3.org/TR/css-text-decor-3/#line-decoration"
+  }]
 }
 !*/
-define(['Modernizr', 'testProp'], function(Modernizr, testProp) {
-  Modernizr.addTest('skipInk', testProp('textDecorationSkipInk', 'auto'));
+define(['Modernizr', 'testAllProps'], function(Modernizr, testAllProps) {
+
+  (function() {
+
+    Modernizr.addTest('textdecoration', function() {
+      var bool = false;
+      var test = testAllProps('textDecoration');
+      try {
+        bool = !!test;
+        if (bool) {
+          bool = new Boolean(bool);
+        }
+      } catch (e) {}
+
+      return bool;
+    });
+
+    var props = ['Line', 'Style', 'Color', 'Skip', 'SkipInk'];
+    var name, test;
+
+    for (var i = 0; i < props.length; i++) {
+      name = props[i].toLowerCase();
+      test = testAllProps('textDecoration' + props[i]);
+
+      Modernizr.addTest('textdecoration.' + name, test);
+    }
+  })();
+
 });
