8281c32393c4a96639e2c09cc1e1e9c6f9807c08
Frank Lemanschik <frank@dspeed.eu>
2018-10-15 11:37:13 +0100

    Create dynamic_import.js
    Add dynamic imports check

---
+++ feature-detects/es6/dynamic_import.js
@@ -0,0 +1,27 @@
+/*!
+{
+  "name": "ES6 DynamicImport",
+  "property": "import",
+  "caniuse": "import",
+  "polyfills": ["systemjs"],
+  "authors": ["Frank Lemanschik"],
+  "tags": ["es6"],
+  "notes": [{
+    "name": "The ES6 Dynamic Import spec",
+    "href": "https://developers.google.com/web/updates/2017/11/dynamic-import"
+  }]
+}
+!*/
+/* DOC
+Check if browser implements ECMAScript 6 Dynamic Import per usage.
+*/
+define(['Modernizr'], function(Modernizr) {
+  Modernizr.addTest('dynamic-import', function() {
+    try {
+		  new Function('import("")');
+		  return true;
+		} catch (err) {
+			return false;
+		}
+  });
+});
