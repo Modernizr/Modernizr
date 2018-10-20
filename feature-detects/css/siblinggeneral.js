2971f0429a9d78beb19992f0c43de51e81951207
Veeck <github@veeck.de>
2018-10-20 19:46:22 +0200

    Fix test for siblinggeneral

--- feature-detects/css/siblinggeneral.js
+++ feature-detects/css/siblinggeneral.js
@@ -1,8 +1,8 @@
 /*!
 {
   "name": "CSS general sibling selector",
-  "caniuse": "css-sel3",
   "property": "siblinggeneral",
+  "caniuse": "css-sel3",
   "tags": ["css"],
   "notes": [{
     "name": "Related Github Issue",
@@ -13,7 +13,7 @@
 define(['Modernizr', 'createElement', 'testStyles'], function(Modernizr, createElement, testStyles) {
   Modernizr.addTest('siblinggeneral', function() {
     return testStyles('#modernizr div {width:100px} #modernizr div ~ div {width:200px;display:block}', function(elem) {
-      return elem.lastChild.offsetWidth === 200;
+      return elem.childNodes[1].offsetWidth === 200;
     }, 2);
   });
 });
