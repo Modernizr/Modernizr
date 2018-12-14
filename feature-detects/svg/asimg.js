cd95a218b5a7b35c479257452768193703491845
veeck <veeck@neofonie.de>
2018-12-14 13:24:02 +0100

    Fix #2386, test doesnt like https urls

--- feature-detects/svg/asimg.js
+++ feature-detects/svg/asimg.js
@@ -20,8 +20,8 @@ define(['Modernizr', 'addTest'], function(Modernizr) {
   // Now a Sync test based on good results here
   // https://codepen.io/chriscoyier/pen/bADFx
 
-  // Note https://www.w3.org/TR/SVG11/feature#Image is *supposed* to represent
+  // Note http://www.w3.org/TR/SVG11/feature#Image is *supposed* to represent
   // support for the `<image>` tag in SVG, not an SVG file linked from an `<img>`
   // tag in HTML – but it’s a heuristic which works
-  Modernizr.addTest('svgasimg', document.implementation.hasFeature('https://www.w3.org/TR/SVG11/feature#Image', '1.1'));
+  Modernizr.addTest('svgasimg', document.implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#Image', '1.1'));
 });
