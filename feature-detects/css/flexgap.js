fee75dfaebac921b86548ff88cb1fcbc76855702
Chris Smith <chris13524@users.noreply.github.com>
2019-12-10 16:57:07 -0500

    Fix linting errors

--- feature-detects/css/flexgap.js
+++ feature-detects/css/flexgap.js
@@ -11,16 +11,16 @@
 }
 !*/
 define(['Modernizr'], function(Modernizr) {
-  Modernizr.addTest("flexgap", function() {
+  Modernizr.addTest('flexgap', function() {
   	// create flex container with row-gap set
-  	var flex = document.createElement("div");
-	  flex.style.display = "flex";
-	  flex.style.flexDirection = "column";
-	  flex.style.rowGap = "1px";
+  	var flex = document.createElement('div');
+	  flex.style.display = 'flex';
+	  flex.style.flexDirection = 'column';
+	  flex.style.rowGap = '1px';
     
   	// create two, elements inside it
-	  flex.appendChild(document.createElement("div"));
-	  flex.appendChild(document.createElement("div"));
+	  flex.appendChild(document.createElement('div'));
+	  flex.appendChild(document.createElement('div'));
     
 	  // append to the DOM (needed to obtain scrollHeight)
 	  document.documentElement.appendChild(flex);
