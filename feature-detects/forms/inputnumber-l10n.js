604fd449e79e09850bc644efa9981e5976bdc349
Guilherme Maron <guilherme.maron@thema.inf.br>
2020-02-04 09:58:27 -0300

    Added top: 0 style to dummy input as in hyphens.js test.

--- feature-detects/forms/inputnumber-l10n.js
+++ feature-detects/forms/inputnumber-l10n.js
@@ -31,7 +31,7 @@ define(['Modernizr', 'createElement', 'getBody', 'test/inputtypes', 'test/forms/
 
     body.insertBefore(div, firstChild);
 
-    div.innerHTML = '<input type="number" value="1.0" step="0.1" style="position: fixed;" />';
+    div.innerHTML = '<input type="number" value="1.0" step="0.1" style="position: fixed; top: 0;" />';
     var input = div.childNodes[0];
     body.appendChild(div);
 
