22a6ed2fbc67968df2843b0c4783f9ba5a059c74
David BRETIN <dabretin@users.noreply.github.com>
2020-03-13 12:24:07 +0100

    Indentation format

--- feature-detects/es6/rest-parameters.js
+++ feature-detects/es6/rest-parameters.js
@@ -10,13 +10,13 @@
 Check if browser implements ECMAScript 6 Rest parameters per specification.
 */
 define(['Modernizr'], function(Modernizr) {
-	Modernizr.addTest('restparameters', function() {
-		try {
-			// eslint-disable-next-line
-			eval('function f(...rest) {}');
-		} catch (e) {
-			return false;
-		}
-		return true;
-	});
+  Modernizr.addTest('restparameters', function() {
+    try {
+      // eslint-disable-next-line
+      eval('function f(...rest) {}');
+    } catch (e) {
+      return false;
+    }
+    return true;
+  });
 });
