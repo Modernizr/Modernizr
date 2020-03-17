48afbc3e6b2191d9608873ab8a37b56b065dbb5f
David BRETIN <dabretin@users.noreply.github.com>
2020-03-17 10:24:20 +0100

    Add spec link

--- feature-detects/es6/rest-parameters.js
+++ feature-detects/es6/rest-parameters.js
@@ -2,6 +2,10 @@
 {
   "name": "ES6 Rest parameters",
   "property": "restparameters",
+  "notes": [{
+    "name": "ECMAScript 6 language specification",
+    "href": "https://www.ecma-international.org/ecma-262/6.0/#sec-function-definitions"
+  }],
   "caniuse": "rest",
   "authors": ["dabretin"],
   "tags": ["es6"]
