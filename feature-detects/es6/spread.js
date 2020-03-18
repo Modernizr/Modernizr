a0d67e0893aea781ab482187c5c9dbc527a1e856
David BRETIN <dabretin@users.noreply.github.com>
2020-03-18 10:38:46 +0100

    ES6 Spread check

---
+++ feature-detects/es6/spread.js
@@ -0,0 +1,32 @@
+/*!
+{
+  "name": "ES6 Spread",
+  "property": "spread",
+  "notes": [{
+    "name": "ECMAScript 6 language specification",
+    "href": "https://tc39.es/ecma262/#sec-array-initializer"
+  },
+  {
+    "name": "Article",
+    "href": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax"
+  }],
+  "caniuse": "spread",
+  "authors": ["dabretin"],
+  "tags": ["es6"]
+}
+!*/
+/* DOC
+Check if browser implements ECMAScript 6 spread syntax (in array and function calls)
+WARNING: not for object literals (=> ES7) 
+*/
+define(['Modernizr'], function(Modernizr) {
+  Modernizr.addTest('spread', function() {
+    try {
+      // eslint-disable-next-line
+      eval('(function f(){})(...[1])');
+    } catch (e) {
+      return false;
+    }
+    return true;
+  });
+});
