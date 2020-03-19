46efd67df34656ad1609f2ebcff886774b2920c0
David BRETIN <dabretin@users.noreply.github.com>
2020-03-19 19:27:23 +0100

    ES7 - rest destructuring (#2517)
    * ES7 - rest destructuring
    
    * Modify 'config-all.json' for adding "es7/rest-destructuring"
    
    * Indentation correction
    
    * Create 2 checks for array and object
    
    * Correct caniuse tag (cannot be a string array)

---
+++ feature-detects/es7/rest-destructuring.js
@@ -0,0 +1,37 @@
+/*!
+{
+  "name": "ES7 Rest destructuring",
+  "property": [ "restdestructuringarray", "restdestructuringobject" ],
+  "caniuse" : "destructuring%20assignment",
+  "notes": [{
+    "name": "official ECMAScript 7 Destructuring Assignment draft specification",
+    "href": "https://tc39.es/ecma262/#sec-destructuring-assignment"
+  }],
+  "authors": ["dabretin"],
+  "warnings": ["ECMAScript 7 is still a only a draft, so this detect may not match the final specification or implementations."],
+  "tags": ["es7"]
+}
+!*/
+/* DOC
+Check if browser implements ECMAScript 7 Destructuring Assignment per specification.
+*/
+define(['Modernizr'], function(Modernizr) {
+  Modernizr.addTest('restdestructuringarray', function() {
+    try {
+      // eslint-disable-next-line
+      eval('var [...rest]=[1]');
+    } catch (e) {
+      return false;
+    }
+    return true;
+  });
+  Modernizr.addTest('restdestructuringobject', function() {
+    try {
+      // eslint-disable-next-line
+      eval('var {...rest}={a:1}');
+    } catch (e) {
+      return false;
+    }
+    return true;
+  });
+});
