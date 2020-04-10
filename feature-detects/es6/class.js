5a784e7978727cc1d37c752544cd3347485fd091
David BRETIN <dabretin@users.noreply.github.com>
2020-04-10 12:26:55 +0200

    ES6: class (#2532)
    * Create es6/class.js
    
    * Modify 'config-all.json' for adding "es6/class"

---
+++ feature-detects/es6/class.js
@@ -0,0 +1,27 @@
+/*!
+{
+  "name": "ES6 Class",
+  "property": "class",
+  "notes": [{
+    "name": "ECMAScript 6 language specification",
+    "href": "https://www.ecma-international.org/ecma-262/6.0/#sec-class-definitions"
+  }],
+  "caniuse": "es6-class",
+  "authors": ["dabretin"],
+  "tags": ["es6"]
+}
+!*/
+/* DOC
+Check if browser implements ECMAScript 6 class.
+*/
+define(['Modernizr'], function(Modernizr) {
+  Modernizr.addTest('class', function() {
+    try {
+      // eslint-disable-next-line
+      eval('class A{}');
+    } catch (e) {
+      return false;
+    }
+    return true;
+  });
+});
