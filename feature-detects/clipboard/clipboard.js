c43b450b8cc1b8982423156f6af56116f4469317
Markel F <github@markel.dev>
2020-06-13 23:42:12 +0200

    Implement sub-modules (but badly)

--- feature-detects/clipboard/clipboard.js
+++ feature-detects/clipboard/clipboard.js
@@ -18,15 +18,56 @@
 /* DOC
 It tests for the whole clipboard API. For sub-features check other tests
 */
-define(['Modernizr', 'addTest'], function(Modernizr, addTest) {
+define(['Modernizr', 'addTest', 'testAllProps'], function(Modernizr, addTest, _testAllProps) {
   Modernizr.addAsyncTest(function() {
-    var result;
+    var result, name;
+    // var props = ['read', 'readText', 'write', 'writeText'];
     if (navigator.clipboard) {
-      result = true;
-    } else {
-      result = false;
+      addTest('clipboard', true);
+      /*
+      // The sub-modules checked only if API is available to avoid Edge crashes
+      for (var i = 0; i < props.length; i++) {
+        if (navigator.clipboard[props[i]]) {
+            result = true;
+        } else {
+            result = false;
+        }
+        name = props[i].toLowerCase();
+        Modernizr.addTest('clipboard.' + name, result);
+      }
+      */
+
+      // A work around while I complete the clean version
+      if (navigator.clipboard.read) {
+        result = true;
+      } else {
+        result = false;
+      }
+      addTest('clipboard.read', result);
+
+      if (navigator.clipboard.readText) {
+        result = true;
+      } else {
+        result = false;
+      }
+      addTest('clipboard.readText', result);
+
+      if (navigator.clipboard.write) {
+        result = true;
+      } else {
+        result = false;
+      }
+      addTest('clipboard.write', result);
+
+      if (navigator.clipboard.writeText) {
+        result = true;
+      } else {
+        result = false;
+      }
+      addTest('clipboard.writeText', result);
     }
-    addTest('clipboard', result);
-    return;
+    else {
+      addTest('clipboard', false);
+    }
   });
 });
