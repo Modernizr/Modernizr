5c129cbfbc80245c526dbfbe73a527c028eb1be6
chris13524 <chris@smith.xyz>
2020-08-25 11:20:22 -0400

    Support display:none iframes in flexgap test

--- feature-detects/css/flexgap.js
+++ feature-detects/css/flexgap.js
@@ -12,22 +12,35 @@
 }
 !*/
 define(['Modernizr', 'createElement', 'docElement'], function(Modernizr, createElement, docElement) {
-  Modernizr.addTest('flexgap', function() {
-    // create flex container with row-gap set
-    var flex = createElement('div');
-    flex.style.display = 'flex';
-    flex.style.flexDirection = 'column';
-    flex.style.rowGap = '1px';
+  Modernizr.addAsyncTest(function() {
+    function doWork() {
+      Modernizr.addTest('flexgap', function() {
+        // create flex container with row-gap set
+        var flex = createElement('div');
+        flex.style.display = 'flex';
+        flex.style.flexDirection = 'column';
+        flex.style.rowGap = '1px';
 
-    // create two elements inside it
-    flex.appendChild(createElement('div'));
-    flex.appendChild(createElement('div'));
+        // create two elements inside it
+        flex.appendChild(createElement('div'));
+        flex.appendChild(createElement('div'));
 
-    // append to the DOM (needed to obtain scrollHeight)
-    docElement.appendChild(flex);
-    var isSupported = flex.scrollHeight === 1; // flex container should be 1px high from the row-gap
-    flex.parentNode.removeChild(flex);
+        // append to the DOM (needed to obtain scrollHeight)
+        docElement.appendChild(flex);
+        var isSupported = flex.scrollHeight === 1; // flex container should be 1px high from the row-gap
+        flex.parentNode.removeChild(flex);
 
-    return isSupported;
+        return isSupported;
+      });
+    }
+
+    if (getComputedStyle(docElement).height != "") {
+      setTimeout(() => doWork()); // seems Modernizr requires the addTest() call to actually be async
+    } else {
+      // no styles, we must be inside a display:none iframe
+      window.addEventListener('resize', function() {
+        doWork();
+      });
+    }
   });
 });
