4591039a77161836fe308f318fbf5f8c5cde6ec7
ricknjacky <jaikishanbrijwani@gmail.com>
2020-10-03 20:01:07 +0530

    added the metadata

--- feature-detects/mediastream.js
+++ feature-detects/mediastream.js
@@ -1,3 +1,22 @@
+/*!
+{
+  "name": "MediaRecorder API",
+  "property": "mediarecorder",
+  "caniuse": "mediarecorder",
+  "tags": ["mediarecorder"],
+  "authors": ["Jaikishan Brijwani"],
+  "notes": [{
+    "name": "MDN Docs",
+    "href": "https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API"
+  }],
+  "polyfills": ["MediaStream Recording API", "MediaStreamRecordingAPI"]
+}
+!*/
+/* DOC
+Detects support for the MediaStream Recording API for detecting mediastreaming.
+*/
+
+
 define(['Modernizr'], function (Modernizr) {
     Modernizr.addTest('mediarecorder', typeof MediaRecorder);
 })
