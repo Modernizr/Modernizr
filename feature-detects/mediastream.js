a5c48b3bb5146d0d30b3c9461a63323b3c6359ca
ricknjacky <jaikishanbrijwani@gmail.com>
2020-10-03 11:03:34 +0530

    MediaRecorder API

--- feature-detects/mediastream.js
+++ feature-detects/mediastream.js
@@ -1,12 +1,3 @@
 define(['Modernizr'], function (Modernizr) {
     Modernizr.addTest('mediarecorder', typeof MediaRecorder);
-    typeof MediaRecorder !== undefined;
-
-
-    if (MediaRecorder) {
-        return true;
-    }
-    else {
-        return false;
-    }
 })
