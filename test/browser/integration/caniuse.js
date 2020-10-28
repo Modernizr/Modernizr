e48c573eebb9f07650d03d030f7370ffde9f2a5d
Tan Zhen Yong <Xenonym@users.noreply.github.com>
2020-10-28 21:32:38 +0800

    Add feature detection for IndexedDB 2.0 (#2607)

--- test/browser/integration/caniuse.js
+++ test/browser/integration/caniuse.js
@@ -93,6 +93,7 @@ window.caniusecb = function(caniuse) {
       hsla: 'css3-colors',
       htmlimports: 'imports',
       indexeddb: 'indexeddb',
+      indexeddb2: 'indexeddb2',
       inlinesvg: 'svg-html5',
       inputtypes: 'forms',
       intersectionobserver: 'intersectionobserver',
