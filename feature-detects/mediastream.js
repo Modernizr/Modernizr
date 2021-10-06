163947825961a8df75a089e4e152af0e64553057
Onkar Dahale <dahaleonkar@gmail.com>
2021-10-06 22:46:39 +0530

    Added metadata to mediastream.js

--- feature-detects/mediastream.js
+++ feature-detects/mediastream.js
@@ -1,5 +1,18 @@
-/*
-METADATA
+/*!
+{
+  "name": "MediaRecorder API",
+  "property": "mediarecorder",
+  "caniuse": "mediarecorder",
+  "tags": ["mediarecorder"],
+  "authors": ["Onkar Dahale"],
+  "notes": [{
+    "name": "MDN Docs",
+    "href": "https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API"
+  }]
+}
+!*/
+/* DOC
+Detects support for the MediaStream Recording API for detecting mediastreaming.
 */
 
 define(['Modernizr'], function (Modernizr) {
