532f686eeb537f91bbc9af3478003e7875de27aa
rejas <github@veeck.de>
2021-10-09 09:25:55 +0200

    Update scripts and dependencies

--- scripts/generate-license.js
+++ scripts/generate-license.js
@@ -1,9 +1,9 @@
-var fs = require('fs');
-var path = require('path');
-var pkg = require('../package.json');
-var licensePath = path.resolve(__dirname, '../LICENSE.md');
+const fs = require('fs');
+const path = require('path');
+const pkg = require('../package.json');
+const licensePath = path.resolve(__dirname, '../LICENSE');
 
-var license = fs.readFileSync(licensePath, 'utf8');
-var data = license.replace(/Modernizr\s(.+?)\s\|/, 'Modernizr ' + pkg.version + ' |');
+const license = fs.readFileSync(licensePath, 'utf8');
+const data = license.replace(/Modernizr\s(.+?)\s\|/, 'Modernizr ' + pkg.version + ' |');
 
 fs.writeFileSync(licensePath, data, 'utf8');
