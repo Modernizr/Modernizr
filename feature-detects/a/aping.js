000d56f401f197642b34140caf2c83b8361b2725
Hélio Correia <dev@heliocorreia.com>
2021-10-10 01:43:39 +0100

    Add a|area[ping] detect

---
+++ feature-detects/a/aping.js
@@ -0,0 +1,19 @@
+/*!
+{
+  "name": "a[ping] Attribute",
+  "property": "aping",
+  "caniuse": "ping",
+  "tags": ["media", "attribute"],
+  "builderAliases": ["a_ping"],
+  "notes": [{
+    "name": "WHATWG Spec",
+    "href": "https://html.spec.whatwg.org/dev/links.html#ping"
+  }]
+}
+!*/
+/* DOC
+The ping attribute, if present, gives the URLs of the resources that are interested in being notified if the user follows the hyperlink.
+*/
+define(['Modernizr', 'createElement'], function(Modernizr, createElement) {
+  Modernizr.addTest('aping', !window.externalHost && 'ping' in createElement('a'));
+});
---
+++ feature-detects/a/areaping.js
@@ -0,0 +1,19 @@
+/*!
+{
+  "name": "area[ping] Attribute",
+  "property": "areaping",
+  "caniuse": "ping",
+  "tags": ["media", "attribute"],
+  "builderAliases": ["area_ping"],
+  "notes": [{
+    "name": "WHATWG Spec",
+    "href": "https://html.spec.whatwg.org/dev/links.html#ping"
+  }]
+}
+!*/
+/* DOC
+The ping attribute, if present, gives the URLs of the resources that are interested in being notified if the user follows the hyperlink.
+*/
+define(['Modernizr', 'createElement'], function(Modernizr, createElement) {
+  Modernizr.addTest('areaping', !window.externalHost && 'ping' in createElement('area'));
+});
