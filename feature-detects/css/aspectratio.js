3959b784a67c0728bbbe0a0fab35638aba0b3230
HelKyle <helkylefighting@gmail.com>
2025-07-15 09:14:16 +0800

    chore: keep the same code style

--- feature-detects/css/aspectratio.js
+++ feature-detects/css/aspectratio.js
@@ -18,19 +18,19 @@ https://developer.mozilla.org/en-US/docs/Web/CSS/aspect-ratio
 */
 define(['Modernizr', 'createElement'], function (Modernizr, createElement) {
   Modernizr.addTest("aspectratio", function () {
-    if (typeof CSS === "object" && typeof CSS.supports === "function") {
-      return CSS.supports('aspect-ratio', '1 / 1')
+    if (CSS && CSS.supports && CSS.supports('aspect-ratio', '1 / 1')) {
+      return true;
+    }
+
+    var element = createElement('p'),
+          elStyle = element.style
+    if ('aspectRatio' in elStyle) {
+      elStyle.cssText = 'aspect-ratio:1 / 1'
+      element.remove()
+      return (elStyle['aspectRatio'] === '1 / 1');
     } else {
-      var element = createElement('p'),
-            elStyle = element.style
-      if ('aspectRatio' in elStyle) {
-        elStyle.cssText = 'aspect-ratio:1 / 1'
-        element.remove()
-        return (elStyle['aspectRatio'] === '1 / 1');
-      } else {
-        element.remove();
-        return false;
-      }
+      element.remove();
+      return false;
     }
   });
 });
